9571b71c2df8803a53d791fce0bcd742e2532be9
WLefever <wouter.lefever@cegeka.com>
2022-05-18 09:50:58 +0200

    fix: Bom issues

--- ldes-server-infra-mongo/mongo-repository-spring-boot-autoconfigure/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/SpringContextTest.java
+++ ldes-server-infra-mongo/mongo-repository-spring-boot-autoconfigure/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/SpringContextTest.java
@@ -1,21 +1,13 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo;
 
-import org.junit.Test;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = MongoAutoConfiguration.class)
-public class SpringContextTest {
-	
-	public SpringContextTest() {
-		
-	}
+class SpringContextTest {
 
     @Test
-    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    void whenSpringContextIsBootstrapped_thenNoExceptions() {
     }
 }
