a579020b19f2b374699e0cf4c92294d2fc0eddd3
WLefever-Cegeka <104509963+WLefever-Cegeka@users.noreply.github.com>
2022-09-29 17:06:27 +0200

    Fix: issue 242: Content-Type return (#247)

--- ldes-server-port-fetch-rest/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/rest/eventstream/EventStreamControllerTest.java
+++ ldes-server-port-fetch-rest/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/rest/eventstream/EventStreamControllerTest.java
@@ -1,6 +1,7 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.rest.eventstream;
 
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.config.LdesConfig;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.constants.RdfConstants;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.converter.PrefixAdder;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.converter.PrefixAdderImpl;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.converter.RdfModelConverter;
@@ -9,10 +10,15 @@ import be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.service
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.services.EventStreamFetcher;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.valueobjects.EventStream;
 import be.vlaanderen.informatievlaanderen.ldes.server.rest.eventstream.config.EventStreamWebConfig;
-import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.*;
 import org.apache.jena.riot.Lang;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -25,7 +31,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
+import static be.vlaanderen.informatievlaanderen.ldes.server.domain.constants.RdfConstants.LDES_EVENT_STREAM_URI;
+import static be.vlaanderen.informatievlaanderen.ldes.server.domain.constants.RdfConstants.TREE_NODE_RESOURCE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -48,18 +59,30 @@ class EventStreamControllerTest {
 	@Autowired
 	LdesConfig ldesConfig;
 
-	@Test
-	void when_GetRequestOnCollectionName_EventStreamIsReturned() throws Exception {
+	@ParameterizedTest(name = "Correct getting of an EventStream from the REST Service with mediatype{0}")
+	@ArgumentsSource(MediaTypeRdfFormatsArgumentsProvider.class)
+	void when_GetRequestOnCollectionName_EventStreamIsReturned(String mediaType, Lang lang) throws Exception {
 		when(eventStreamFetcher.fetchEventStream()).thenReturn(
 				new EventStream("collection", "timestampPath", "versionOf", "shape", List.of("viewOne", "viewTwo")));
 		ResultActions resultActions = mockMvc.perform(get("/{viewName}",
 				ldesConfig.getCollectionName())
-				.accept("application/n-quads")).andDo(print())
+				.accept(mediaType)).andDo(print())
 				.andExpect(status().isOk());
 		Model actualModel = RdfModelConverter.fromString(resultActions.andReturn().getResponse().getContentAsString(),
-				Lang.NQUADS);
-		Model expectedModel = getExpectedModel();
-		assertTrue(expectedModel.isIsomorphicWith(actualModel));
+				lang);
+		assertEquals(LDES_EVENT_STREAM_URI, getObjectURI(actualModel,
+				RdfConstants.RDF_SYNTAX_TYPE));
+	}
+
+	private String getObjectURI(Model model, Property property) {
+		return model
+				.listStatements(null, property, (Resource) null)
+				.nextOptional()
+				.map(Statement::getObject)
+				.map(RDFNode::asResource)
+				.map(Resource::getURI)
+				.map(Objects::toString)
+				.orElse(null);
 	}
 
 	@Test
@@ -83,6 +106,18 @@ class EventStreamControllerTest {
 						<http://localhost:8080/collection> <https://w3id.org/tree#shape> <shape> .
 						<http://localhost:8080/collection> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/ldes#EventStream> .""",
 				Lang.NQUADS);
+	}
+
+	static class MediaTypeRdfFormatsArgumentsProvider implements
+			ArgumentsProvider {
+
+		@Override
+		public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+			return Stream.of(
+					Arguments.of("application/n-quads", Lang.NQUADS),
+					Arguments.of("application/turtle", Lang.TURTLE),
+					Arguments.of("*/*", Lang.TURTLE));
+		}
 	}
 
 	@TestConfiguration
