e8774bd3e30a5a55ab5adba05d6f94e4d6c8ca30
WLefever <wouter.lefever@cegeka.com>
2022-11-09 14:54:04 +0100

    Feature: VSDSPUB-349: Add TimeBasedRetentionPolicy

---
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/ldes/retentionpolicy/config/TimebasedRetentionProperties.java
@@ -0,0 +1,9 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.retentionpolicy.config;
+
+public class TimebasedRetentionProperties {
+	private TimebasedRetentionProperties() {
+	}
+
+	public static final String DURATION_IN_SECONDS = "durationInSeconds";
+
+}
