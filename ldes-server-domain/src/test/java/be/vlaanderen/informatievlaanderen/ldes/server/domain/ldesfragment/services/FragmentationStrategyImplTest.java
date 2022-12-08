90341471e4dc8f11cdcf199780b4a5e5a3ae0d24
WLefever <wouter.lefever@cegeka.com>
2022-12-08 11:21:46 +0100

    Fix FragmentationStrategyImplTest

--- ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/ldesfragment/services/FragmentationStrategyImplTest.java
+++ ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/ldesfragment/services/FragmentationStrategyImplTest.java
@@ -1,51 +1,36 @@
-// package
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.services;
-//
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.NonCriticalTasksExecutor;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.entities.LdesFragment;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.repository.LdesFragmentRepository;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.valueobjects.FragmentInfo;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entities.Member;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.repository.MemberRepository;
-// import org.junit.jupiter.api.Test;
-//
-// import java.util.List;
-//
-// import static org.mockito.Mockito.*;
-//
-// class FragmentationStrategyImplTest {
-// private final LdesFragmentRepository ldesFragmentRepository =
-// mock(LdesFragmentRepository.class);
-// private final MemberRepository memberRepository =
-// mock(MemberRepository.class);
-// private final NonCriticalTasksExecutor nonCriticalTasksExecutor =
-// mock(NonCriticalTasksExecutor.class);
-//
-// private final FragmentationStrategyImpl fragmentationStrategy = new
-// FragmentationStrategyImpl(
-// ldesFragmentRepository,
-// memberRepository, nonCriticalTasksExecutor);
-//
-// @Test
-// void
-// when_memberIsAddedToFragment_FragmentationStrategyImplSavesUpdatedFragment()
-// {
-// LdesFragment ldesFragment = new LdesFragment(new FragmentInfo("view",
-// List.of()));
-// Member member = mock(Member.class);
-// when(member.getLdesMemberId()).thenReturn("memberId");
-//
-// fragmentationStrategy.addMemberToFragment(ldesFragment, member, any());
-//
-// verify(ldesFragmentRepository, times(1)).saveFragment(ldesFragment);
-// // verify(memberRepository, times(1)).saveMemberReference("memberId",
-// "/view");
-// // assertEquals(List.of("memberId"), ldesFragment.getMemberIds());
-// }
-// }
+package be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.services;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.NonCriticalTasksExecutor;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.entities.LdesFragment;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.repository.LdesFragmentRepository;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.valueobjects.FragmentInfo;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entities.Member;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class FragmentationStrategyImplTest {
+	private final LdesFragmentRepository ldesFragmentRepository = mock(LdesFragmentRepository.class);
+	private final MemberRepository memberRepository = mock(MemberRepository.class);
+	private final NonCriticalTasksExecutor nonCriticalTasksExecutor = mock(NonCriticalTasksExecutor.class);
+
+	private final FragmentationStrategyImpl fragmentationStrategy = new FragmentationStrategyImpl(
+			ldesFragmentRepository,
+			memberRepository, nonCriticalTasksExecutor);
+
+	@Test
+	void when_memberIsAddedToFragment_FragmentationStrategyImplSavesUpdatedFragment() {
+		LdesFragment ldesFragment = new LdesFragment(new FragmentInfo("view",
+				List.of()));
+		Member member = mock(Member.class);
+		when(member.getLdesMemberId()).thenReturn("memberId");
+
+		fragmentationStrategy.addMemberToFragment(ldesFragment, member, any());
+
+		verify(nonCriticalTasksExecutor, times(1)).submit(any(Runnable.class));
+		verify(ldesFragmentRepository, times(1)).incrementNumberOfMembers(ldesFragment.getFragmentId());
+	}
+}
