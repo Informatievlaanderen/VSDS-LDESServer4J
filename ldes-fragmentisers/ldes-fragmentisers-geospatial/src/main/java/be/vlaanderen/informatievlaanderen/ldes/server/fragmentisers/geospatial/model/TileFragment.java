ded731b377eb8e6fc7bcaad268efe6034f0636d4
WLefever <wouter.lefever@cegeka.com>
2022-12-08 13:14:59 +0100

    Remove TileFragment.java

--- ldes-fragmentisers/ldes-fragmentisers-geospatial/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/geospatial/model/TileFragment.java
+++
@@ -1,5 +0,0 @@
-package be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial.model;
-
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.entities.LdesFragment;
-
-public record TileFragment(LdesFragment ldesFragment, boolean created) {}
