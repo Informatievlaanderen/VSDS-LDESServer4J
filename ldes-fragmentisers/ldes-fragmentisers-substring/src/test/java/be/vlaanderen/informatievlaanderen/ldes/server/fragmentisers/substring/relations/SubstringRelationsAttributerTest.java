f4a3e16b673bbfaffb6b221ee276987061e7dfc0
WLefever <wouter.lefever@cegeka.com>
2022-12-12 15:19:51 +0100

    rebase

--- ldes-fragmentisers/ldes-fragmentisers-substring/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/substring/relations/SubstringRelationsAttributerTest.java
+++ ldes-fragmentisers/ldes-fragmentisers-substring/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/substring/relations/SubstringRelationsAttributerTest.java
@@ -30,7 +30,7 @@ class SubstringRelationsAttributerTest {
 		TreeRelationsRepository treeRelationsRepository = mock(TreeRelationsRepository.class);
 		nonCriticalTasksExecutor = mock(NonCriticalTasksExecutor.class);
 		SubstringConfig substringConfig = new SubstringConfig();
-		substringConfig.setFragmenterSubjectFilter("somefilter");
+		substringConfig.setFragmenterPropertyQuery("somefilter");
 		substringRelationsAttributer = new SubstringRelationsAttributer(treeRelationsRepository,
 				nonCriticalTasksExecutor, substringConfig);
 	}
@@ -44,63 +44,3 @@ class SubstringRelationsAttributerTest {
 
 	}
 }
-// import org.junit.jupiter.api.Test;
-//
-// import java.util.List;
-//
-// import static
-// be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.substring.fragment.SubstringFragmentCreator.SUBSTRING;
-// import static org.mockito.Mockito.*;
-//
-// class SubstringRelationsAttributerTest {
-// private LdesFragmentRepository ldesFragmentRepository;
-// private SubstringRelationsAttributer substringRelationsAttributer;
-//
-// private static LdesFragment PARENT_FRAGMENT;
-// private static LdesFragment CHILD_FRAGMENT;
-// private static final String VIEW_NAME = "view";private static final String FRAGMENTER_PROPERTY = "fragmenter#property";
-//
-// private static final TreeRelation EXPECTED_RELATION = new TreeRelation(FRAGMENTER_PROPERTY,
-// "/view?substring=ab",
-// "ab",
-// "http://www.w3.org/2001/XMLSchema#string",
-// "https://w3id.org/tree#SubstringRelation");
-//
-// @BeforeEach
-// void setUp() {
-// PARENT_FRAGMENT = new LdesFragment(
-// new FragmentInfo(VIEW_NAME, List.of()));
-// CHILD_FRAGMENT = new LdesFragment(
-// new FragmentInfo(VIEW_NAME, List.of(new FragmentPair(SUBSTRING, "ab"))));
-//
-// ldesFragmentRepository = mock(LdesFragmentRepository.class);
-// substringRelationsAttributer = new
-// SubstringRelationsAttributer(ldesFragmentRepository,
-// treeNodeRelationsRepository);SubstringConfig substringConfig = mock(SubstringConfig.class);
-		when(substringConfig.getFragmenterPropertyQuery()).thenReturn(FRAGMENTER_PROPERTY);
-		substringRelationsAttributer = new SubstringRelationsAttributer(ldesFragmentRepository, substringConfig);
-// }
-//
-// @Test
-// void when_ParentHasNotYetRelation_AddRelation() {
-// substringRelationsAttributer.addSubstringRelation(PARENT_FRAGMENT,
-// CHILD_FRAGMENT);
-//
-// assertTrue(CHILD_FRAGMENT.getRelations().isEmpty());
-// assertEquals(List.of(EXPECTED_RELATION), PARENT_FRAGMENT.getRelations());
-// verify(ldesFragmentRepository, times(1)).saveFragment(PARENT_FRAGMENT);
-//
-// }
-//
-// @Test
-// void when_ParentHasAlreadyRelation_DoNotAddRelation() {
-// PARENT_FRAGMENT.addRelation(EXPECTED_RELATION);
-//
-// substringRelationsAttributer.addSubstringRelation(PARENT_FRAGMENT,
-// CHILD_FRAGMENT);
-//
-// assertTrue(CHILD_FRAGMENT.getRelations().isEmpty());
-// assertEquals(List.of(EXPECTED_RELATION), PARENT_FRAGMENT.getRelations());
-// verifyNoInteractions(ldesFragmentRepository);
-// }
-// }
