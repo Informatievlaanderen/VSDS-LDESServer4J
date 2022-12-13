7251c8417490f8c84ce4efebfc70d808e7f3c3de
pj-cegeka <119848850+pj-cegeka@users.noreply.github.com>
2022-12-13 10:56:53 +0100

    Fix/323 improve retrieval of memberid (#331)
    * temp
    
    * fix/ added membertype to MalformedMemberIdException
    
    * fix/ added test for MalformedMemberIdException
    
    * fix/ added test for MalformedMemberIdException
    
    * fix/ formatter
    
    * fix/ Changed MalformedMemberIdException message formatting and removed code smells
    
    * fix/ formatter

--- ldes-server-port-ingestion-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingestion/rest/exceptions/MalformedMemberIdException.java
+++ ldes-server-port-ingestion-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingestion/rest/exceptions/MalformedMemberIdException.java
@@ -2,13 +2,17 @@ package be.vlaanderen.informatievlaanderen.ldes.server.ingestion.rest.exceptions
 
 public class MalformedMemberIdException extends RuntimeException {
 
-	public MalformedMemberIdException() {
+	private final String expectedMemberType;
+
+	public MalformedMemberIdException(String expectedMemberType) {
 		super();
+		this.expectedMemberType = expectedMemberType;
 	}
 
 	@Override
 	public String getMessage() {
-		return "Member id could not be extracted. MemberType could not be found in listStatements.";
+		return "Member id could not be extracted. MemberType " + expectedMemberType
+				+ " could not be found in listStatements.";
 	}
 
 }
