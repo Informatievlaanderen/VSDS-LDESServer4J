7a30136026e2003cde0bf6bf16aad8245207dc76
Stijn Verholen <boro@inuits.eu>
2022-12-19 15:17:07 +0100

    feat: VSDSPUB-110: url strategy: fix error on unknown view

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/tree/node/services/TreeNodeFactoryImpl.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/tree/node/services/TreeNodeFactoryImpl.java
@@ -1,5 +1,9 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.node.services;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.config.LdesConfig;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.MissingFragmentException;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.entities.LdesFragment;
@@ -9,9 +13,6 @@ import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entitie
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.repository.MemberRepository;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.node.entities.TreeNode;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.relations.TreeRelationsRepository;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class TreeNodeFactoryImpl implements TreeNodeFactory {
@@ -31,7 +32,8 @@ public class TreeNodeFactoryImpl implements TreeNodeFactory {
 	public TreeNode getTreeNode(String treeNodeId) {
 		LdesFragment ldesFragment = ldesFragmentRepository.retrieveFragment(treeNodeId)
 				.orElseThrow(
-						() -> new MissingFragmentException(ldesConfig.getHostName() + treeNodeId));
+						() -> new MissingFragmentException(
+								ldesConfig.getHostName() + "/" + ldesConfig.getCollectionName() + treeNodeId));
 		List<TreeRelation> relations = treeRelationsRepository.getRelations(treeNodeId);
 		List<Member> members = memberRepository.getMembersByReference(treeNodeId);
 		return new TreeNode(treeNodeId, ldesFragment.isImmutable(), ldesFragment.isSoftDeleted(), relations, members);
