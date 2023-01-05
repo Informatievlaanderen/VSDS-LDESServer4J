ed967ceb263ff9a50bc367564f08f63ae81ea2fb
Jan Robert <jan.robert@cegeka.com>
2023-01-05 17:46:30 +0100

    fixed tests

--- ldes-fragmentisers/ldes-fragmentisers-geospatial/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/geospatial/GeospatialFragmentationStrategyTest.java
+++ ldes-fragmentisers/ldes-fragmentisers-geospatial/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/geospatial/GeospatialFragmentationStrategyTest.java
@@ -1,103 +1,87 @@
-// package
-// be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial;
-//
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.entities.LdesFragment;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.services.FragmentationStrategy;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.valueobjects.FragmentInfo;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragmentrequest.valueobjects.FragmentPair;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entities.Member;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.relations.TreeRelationsRepository;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial.bucketising.GeospatialBucketiser;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial.fragments.GeospatialFragmentCreator;
-// import io.micrometer.observation.Observation;
-// import io.micrometer.observation.ObservationRegistry;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-//
-// import java.util.List;
-// import java.util.Set;
-//
-// import static
-// be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial.constants.GeospatialConstants.FRAGMENT_KEY_TILE;
-// import static
-// be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial.constants.GeospatialConstants.FRAGMENT_KEY_TILE_ROOT;
-// import static org.mockito.Mockito.*;
-//
-// class GeospatialFragmentationStrategyTest {
-//
-// private static final String VIEW_NAME = "view";
-// private static final LdesFragment PARENT_FRAGMENT = new LdesFragment(new
-// FragmentInfo(VIEW_NAME, List.of()));
-// private static final LdesFragment ROOT_TILE_FRAGMENT = PARENT_FRAGMENT
-// .createChild(new FragmentPair(FRAGMENT_KEY_TILE, FRAGMENT_KEY_TILE_ROOT));
-//
-// private GeospatialBucketiser geospatialBucketiser;
-// private GeospatialFragmentCreator fragmentCreator;
-// private ObservationRegistry observationRegistry;
-// private final TreeRelationsRepository treeRelationsRepository =
-// mock(TreeRelationsRepository.class);
-// private FragmentationStrategy decoratedFragmentationStrategy;
-// private GeospatialFragmentationStrategy geospatialFragmentationStrategy;
-//
-// @BeforeEach
-// void setUp() {
-// geospatialBucketiser = mock(GeospatialBucketiser.class);
-// fragmentCreator = mock(GeospatialFragmentCreator.class);
-// observationRegistry = mock(ObservationRegistry.class);
-// decoratedFragmentationStrategy = mock(FragmentationStrategy.class);
-// when(fragmentCreator.getOrCreateRootFragment(PARENT_FRAGMENT,
-// FRAGMENT_KEY_TILE_ROOT))
-// .thenReturn(ROOT_TILE_FRAGMENT);
-// geospatialFragmentationStrategy = new
-// GeospatialFragmentationStrategy(decoratedFragmentationStrategy,
-// geospatialBucketiser, fragmentCreator, observationRegistry,
-// treeRelationsRepository);
-// }
-//
-// @Test
-// void when_MemberIsAddedToFragment_GeospatialFragmentationIsApplied() {
-// Member member = mock(Member.class);
-// Observation parentObservation = mock(Span.class);
-// Observation childObservation = mock(Span.class);
-// when(observationRegistry.nextSpan(parentSpan)).thenReturn(childSpan);
-// when(childSpan.name("geospatial fragmentation")).thenReturn(childSpan);
-// when(childSpan.start()).thenReturn(childSpan);
-// when(geospatialBucketiser.bucketise(member)).thenReturn(Set.of("1/1/1",
-// "2/2/2", "3/3/3"));
-// LdesFragment tileFragmentOne = mockCreationGeospatialFragment("1/1/1");
-// LdesFragment tileFragmentTwo = mockCreationGeospatialFragment("2/2/2");
-// LdesFragment tileFragmentThree = mockCreationGeospatialFragment("3/3/3");
-//
-// geospatialFragmentationStrategy.addMemberToFragment(PARENT_FRAGMENT, member,
-// parentSpan);
-//
-// verify(decoratedFragmentationStrategy,
-// times(1)).addMemberToFragment(tileFragmentOne,
-// member, childSpan);
-// verify(decoratedFragmentationStrategy,
-// times(1)).addMemberToFragment(tileFragmentTwo,
-// member, childSpan);
-// verify(decoratedFragmentationStrategy,
-// times(1)).addMemberToFragment(tileFragmentThree,
-// member, childSpan);
-// verifyNoMoreInteractions(decoratedFragmentationStrategy);
-// verify(childSpan, times(1)).end();
-// }
-//
-// private LdesFragment mockCreationGeospatialFragment(String tile) {
-// LdesFragment tileFragment = PARENT_FRAGMENT.createChild(new
-// FragmentPair(FRAGMENT_KEY_TILE, tile));
-// when(fragmentCreator.getOrCreateTileFragment(PARENT_FRAGMENT, tile,
-// ROOT_TILE_FRAGMENT))
-// .thenReturn(tileFragment);
-// return tileFragment;
-// }
-// }
+package be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.entities.LdesFragment;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.services.FragmentationStrategy;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.valueobjects.FragmentInfo;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragmentrequest.valueobjects.FragmentPair;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entities.Member;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.relations.TreeRelationsRepository;
+import be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial.bucketising.GeospatialBucketiser;
+import be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial.fragments.GeospatialFragmentCreator;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial.constants.GeospatialConstants.FRAGMENT_KEY_TILE;
+import static be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.geospatial.constants.GeospatialConstants.FRAGMENT_KEY_TILE_ROOT;
+import static org.mockito.Mockito.*;
+
+class GeospatialFragmentationStrategyTest {
+
+	private static final String VIEW_NAME = "view";
+	private static final LdesFragment PARENT_FRAGMENT = new LdesFragment(new
+			FragmentInfo(VIEW_NAME, List.of()));
+	private static final LdesFragment ROOT_TILE_FRAGMENT = PARENT_FRAGMENT
+			.createChild(new FragmentPair(FRAGMENT_KEY_TILE, FRAGMENT_KEY_TILE_ROOT));
+
+	private GeospatialBucketiser geospatialBucketiser;
+	private GeospatialFragmentCreator fragmentCreator;
+	private ObservationRegistry observationRegistry;
+	private final TreeRelationsRepository treeRelationsRepository =
+			mock(TreeRelationsRepository.class);
+	private FragmentationStrategy decoratedFragmentationStrategy;
+	private GeospatialFragmentationStrategy geospatialFragmentationStrategy;
+
+	@BeforeEach
+	void setUp() {
+		geospatialBucketiser = mock(GeospatialBucketiser.class);
+		fragmentCreator = mock(GeospatialFragmentCreator.class);
+		observationRegistry = ObservationRegistry.create();
+		decoratedFragmentationStrategy = mock(FragmentationStrategy.class);
+		when(fragmentCreator.getOrCreateRootFragment(PARENT_FRAGMENT,
+				FRAGMENT_KEY_TILE_ROOT))
+				.thenReturn(ROOT_TILE_FRAGMENT);
+		geospatialFragmentationStrategy = new
+				GeospatialFragmentationStrategy(decoratedFragmentationStrategy,
+				geospatialBucketiser, fragmentCreator, observationRegistry,
+				treeRelationsRepository);
+	}
+
+	@Test
+	void when_MemberIsAddedToFragment_GeospatialFragmentationIsApplied() {
+		Member member = mock(Member.class);
+
+		when(geospatialBucketiser.bucketise(member)).thenReturn(Set.of("1/1/1",
+				"2/2/2", "3/3/3"));
+		LdesFragment tileFragmentOne = mockCreationGeospatialFragment("1/1/1");
+		LdesFragment tileFragmentTwo = mockCreationGeospatialFragment("2/2/2");
+		LdesFragment tileFragmentThree = mockCreationGeospatialFragment("3/3/3");
+
+		geospatialFragmentationStrategy.addMemberToFragment(PARENT_FRAGMENT, member,
+				mock(Observation.class));
+
+		verify(decoratedFragmentationStrategy,
+				times(1)).addMemberToFragment(eq(tileFragmentOne),
+				eq(member), any(Observation.class));
+		verify(decoratedFragmentationStrategy,
+				times(1)).addMemberToFragment(eq(tileFragmentTwo),
+				eq(member), any(Observation.class));
+		verify(decoratedFragmentationStrategy,
+				times(1)).addMemberToFragment(eq(tileFragmentThree),
+				eq(member), any(Observation.class));
+		verifyNoMoreInteractions(decoratedFragmentationStrategy);
+	}
+
+	private LdesFragment mockCreationGeospatialFragment(String tile) {
+		LdesFragment tileFragment = PARENT_FRAGMENT.createChild(new
+				FragmentPair(FRAGMENT_KEY_TILE, tile));
+		when(fragmentCreator.getOrCreateTileFragment(PARENT_FRAGMENT, tile,
+				ROOT_TILE_FRAGMENT))
+				.thenReturn(tileFragment);
+		return tileFragment;
+	}
+}
--- ldes-fragmentisers/ldes-fragmentisers-timebased/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/timebased/TimebasedFragmentationStrategyTest.java
+++ ldes-fragmentisers/ldes-fragmentisers-timebased/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/timebased/TimebasedFragmentationStrategyTest.java
@@ -1,109 +1,87 @@
-// package
-// be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.timebased;
-//
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.entities.LdesFragment;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.services.FragmentationStrategy;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.valueobjects.FragmentInfo;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragmentrequest.valueobjects.FragmentPair;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entities.Member;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.relations.TreeRelationsRepository;
-// import
-// be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.timebased.services.OpenFragmentProvider;
-// import org.apache.commons.lang3.tuple.ImmutablePair;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.mockito.InOrder;
-// import org.springframework.cloud.sleuth.Span;
-// import org.springframework.cloud.sleuth.Tracer;
-//
-// import java.util.List;
-//
-// import static org.mockito.Mockito.*;
-//
-// class TimebasedFragmentationStrategyTest {
-//
-// private static final String VIEW_NAME = "view";
-// private final OpenFragmentProvider openFragmentProvider =
-// mock(OpenFragmentProvider.class);
-// private final FragmentationStrategy decoratedFragmentationStrategy =
-// mock(FragmentationStrategy.class);
-//
-// private final ObservationRegistry tracer = mock(Tracer.class);
-//
-// private FragmentationStrategy fragmentationStrategy;
-// private static LdesFragment PARENT_FRAGMENT;
-// private static LdesFragment OPEN_FRAGMENT;
-// private final TreeRelationsRepository treeRelationsRepository =
-// mock(TreeRelationsRepository.class);
-//
-// @BeforeEach
-// void setUp() {
-// PARENT_FRAGMENT = new LdesFragment(new FragmentInfo(VIEW_NAME, List.of()));
-// OPEN_FRAGMENT = PARENT_FRAGMENT.createChild(new
-// FragmentPair("generatedAtTime", "someTime"));
-// fragmentationStrategy = new
-// TimebasedFragmentationStrategy(decoratedFragmentationStrategy,
-// openFragmentProvider, tracer,
-// treeRelationsRepository);
-// }
-//
-// @Test
-// void when_MemberIsAddedToFragment_TimebasedFragmentationIsApplied() {
-// Member member = mock(Member.class);
-// when(member.getLdesMemberId()).thenReturn("memberId");
-// when(openFragmentProvider.retrieveOpenFragmentOrCreateNewFragment(PARENT_FRAGMENT))
-// .thenReturn(new ImmutablePair<>(OPEN_FRAGMENT, false));
-// Observation parentObservation = mock(Span.class);
-// Observation childObservation = mock(Span.class);
-// when(tracer.nextSpan(parentSpan)).thenReturn(childSpan);
-// when(childSpan.name("timebased fragmentation")).thenReturn(childSpan);
-// when(childSpan.start()).thenReturn(childSpan);
-//
-// fragmentationStrategy.addMemberToFragment(PARENT_FRAGMENT,
-// member, parentSpan);
-//
-// InOrder inOrder = inOrder(openFragmentProvider, treeRelationsRepository,
-// decoratedFragmentationStrategy);
-// inOrder.verify(openFragmentProvider,
-// times(1)).retrieveOpenFragmentOrCreateNewFragment(PARENT_FRAGMENT);
-// inOrder.verify(decoratedFragmentationStrategy,
-// times(1)).addMemberToFragment(OPEN_FRAGMENT, member,
-// childSpan);
-// inOrder.verifyNoMoreInteractions();
-// }
-//
-// @Test
-// void
-// when_MemberIsAddedToFirstFragment_TimebasedFragmentationIsAppliedAndRelationIsAdded()
-// {
-// Member member = mock(Member.class);
-// when(member.getLdesMemberId()).thenReturn("memberId");
-// when(openFragmentProvider.retrieveOpenFragmentOrCreateNewFragment(PARENT_FRAGMENT))
-// .thenReturn(new ImmutablePair<>(OPEN_FRAGMENT, true));
-// Observation parentObservation = mock(Span.class);
-// Observation childObservation = mock(Span.class);
-// when(tracer.nextSpan(parentSpan)).thenReturn(childSpan);
-// when(childSpan.name("timebased fragmentation")).thenReturn(childSpan);
-// when(childSpan.start()).thenReturn(childSpan);
-//
-// fragmentationStrategy.addMemberToFragment(PARENT_FRAGMENT,
-// member, parentSpan);
-//
-// InOrder inOrder = inOrder(openFragmentProvider, treeRelationsRepository,
-// decoratedFragmentationStrategy);
-// inOrder.verify(openFragmentProvider,
-// times(1)).retrieveOpenFragmentOrCreateNewFragment(PARENT_FRAGMENT);
-// inOrder.verify(treeRelationsRepository,
-// times(1)).addTreeRelation(eq(PARENT_FRAGMENT.getFragmentId()), any());
-// inOrder.verify(decoratedFragmentationStrategy,
-// times(1)).addMemberToFragment(OPEN_FRAGMENT, member,
-// childSpan);
-// inOrder.verifyNoMoreInteractions();
-// }
-// }
+package be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.timebased;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.entities.LdesFragment;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.services.FragmentationStrategy;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.valueobjects.FragmentInfo;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragmentrequest.valueobjects.FragmentPair;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entities.Member;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.relations.TreeRelationsRepository;
+import be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.timebased.services.OpenFragmentProvider;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class TimebasedFragmentationStrategyTest {
+
+	private static final String VIEW_NAME = "view";
+	private final OpenFragmentProvider openFragmentProvider =
+			mock(OpenFragmentProvider.class);
+	private final FragmentationStrategy decoratedFragmentationStrategy =
+			mock(FragmentationStrategy.class);
+
+	private FragmentationStrategy fragmentationStrategy;
+	private static LdesFragment PARENT_FRAGMENT;
+	private static LdesFragment OPEN_FRAGMENT;
+	private final TreeRelationsRepository treeRelationsRepository =
+			mock(TreeRelationsRepository.class);
+
+	@BeforeEach
+	void setUp() {
+		PARENT_FRAGMENT = new LdesFragment(new FragmentInfo(VIEW_NAME, List.of()));
+		OPEN_FRAGMENT = PARENT_FRAGMENT.createChild(new
+				FragmentPair("generatedAtTime", "someTime"));
+		fragmentationStrategy = new
+				TimebasedFragmentationStrategy(decoratedFragmentationStrategy,
+				openFragmentProvider, ObservationRegistry.create(),
+				treeRelationsRepository);
+	}
+
+	@Test
+	void when_MemberIsAddedToFragment_TimebasedFragmentationIsApplied() {
+		Member member = mock(Member.class);
+		when(member.getLdesMemberId()).thenReturn("memberId");
+		when(openFragmentProvider.retrieveOpenFragmentOrCreateNewFragment(PARENT_FRAGMENT))
+				.thenReturn(new ImmutablePair<>(OPEN_FRAGMENT, false));
+
+		fragmentationStrategy.addMemberToFragment(PARENT_FRAGMENT,
+				member, any(Observation.class));
+
+		InOrder inOrder = inOrder(openFragmentProvider, treeRelationsRepository,
+				decoratedFragmentationStrategy);
+		inOrder.verify(openFragmentProvider,
+				times(1)).retrieveOpenFragmentOrCreateNewFragment(PARENT_FRAGMENT);
+		inOrder.verify(decoratedFragmentationStrategy,
+				times(1)).addMemberToFragment(eq(OPEN_FRAGMENT), eq(member), any(Observation.class));
+		inOrder.verifyNoMoreInteractions();
+	}
+
+	@Test
+	void
+	when_MemberIsAddedToFirstFragment_TimebasedFragmentationIsAppliedAndRelationIsAdded() {
+		Member member = mock(Member.class);
+		when(member.getLdesMemberId()).thenReturn("memberId");
+		when(openFragmentProvider.retrieveOpenFragmentOrCreateNewFragment(PARENT_FRAGMENT))
+				.thenReturn(new ImmutablePair<>(OPEN_FRAGMENT, true));
+
+		fragmentationStrategy.addMemberToFragment(PARENT_FRAGMENT,
+				member, any(Observation.class));
+
+		InOrder inOrder = inOrder(openFragmentProvider, treeRelationsRepository,
+				decoratedFragmentationStrategy);
+		inOrder.verify(openFragmentProvider,
+				times(1)).retrieveOpenFragmentOrCreateNewFragment(PARENT_FRAGMENT);
+		inOrder.verify(treeRelationsRepository,
+				times(1)).addTreeRelation(eq(PARENT_FRAGMENT.getFragmentId()), any());
+		inOrder.verify(decoratedFragmentationStrategy,
+				times(1)).addMemberToFragment(eq(OPEN_FRAGMENT), eq(member),
+				any(Observation.class));
+		inOrder.verifyNoMoreInteractions();
+	}
+}
