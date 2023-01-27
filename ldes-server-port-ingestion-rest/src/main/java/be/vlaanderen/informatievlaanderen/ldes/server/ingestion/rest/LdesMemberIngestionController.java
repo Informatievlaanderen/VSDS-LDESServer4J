be2bba51b97d21a11a3b39300debc2af065ee6be
Jan Robert <jan.robert@cegeka.com>
2023-01-27 17:01:15 +0100

    feat: VSDSPUB-192: fix broken SHACL validation

--- ldes-server-port-ingestion-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingestion/rest/LdesMemberIngestionController.java
+++ ldes-server-port-ingestion-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingestion/rest/LdesMemberIngestionController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LdesMemberIngestionController {
 
 	@Autowired
-	MemberIngestService memberIngestService;
+	private MemberIngestService memberIngestService;
 	@Autowired
 	private LdesShaclValidator validator;
 
