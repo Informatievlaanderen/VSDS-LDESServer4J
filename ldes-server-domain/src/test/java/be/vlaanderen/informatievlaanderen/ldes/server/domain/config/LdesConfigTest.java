e7cae090c740e71b8860fbe7814cf24a9e891981
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-02-03 10:57:14 +0100

    Run formatter

--- ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/config/LdesConfigTest.java
+++ ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/config/LdesConfigTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = { LdesConfig.class, PathToModelConverter.class })
 @EnableConfigurationProperties
