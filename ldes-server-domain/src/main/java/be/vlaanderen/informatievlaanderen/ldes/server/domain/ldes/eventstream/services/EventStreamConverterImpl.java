f7bda0aa890dcf4dc736f91a44254df995b18497
WLefever <wouter.lefever@cegeka.com>
2023-02-07 16:44:49 +0100

    Isolated TreeRelationResponse

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/ldes/eventstream/services/EventStreamConverterImpl.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/ldes/eventstream/services/EventStreamConverterImpl.java
@@ -3,9 +3,9 @@ package be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.s
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.config.LdesConfig;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.converter.PrefixAdder;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.fetching.EventStreamInfoResponse;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.fetching.TreeRelationResponse;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.valueobjects.EventStream;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.node.entities.TreeNode;
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.relations.services.RelationStatementConverter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
@@ -25,13 +25,10 @@ public class EventStreamConverterImpl implements EventStreamConverter {
 
 	private final PrefixAdder prefixAdder;
 	private final LdesConfig ldesConfig;
-	private final RelationStatementConverter relationStatementConverter;
 
-	public EventStreamConverterImpl(PrefixAdder prefixAdder, LdesConfig ldesConfig,
-			RelationStatementConverter relationStatementConverter) {
+	public EventStreamConverterImpl(PrefixAdder prefixAdder, LdesConfig ldesConfig) {
 		this.prefixAdder = prefixAdder;
 		this.ldesConfig = ldesConfig;
-		this.relationStatementConverter = relationStatementConverter;
 	}
 
 	public Model toModel(final EventStream eventStream) {
@@ -57,9 +54,18 @@ public class EventStreamConverterImpl implements EventStreamConverter {
 		views.forEach(view -> {
 			Resource viewResource = createResource(
 					ldesConfig.getHostName() + "/" + ldesConfig.getCollectionName() + view.getFragmentId());
-
 			statements.add(createStatement(viewResource, RDF_SYNTAX_TYPE, createResource(TREE_NODE_RESOURCE)));
-			statements.addAll(relationStatementConverter.getRelationStatements(view.getRelations(), viewResource));
+			view.getRelations()
+					.forEach(treeRelation -> {
+						TreeRelationResponse treeRelationResponse = new TreeRelationResponse(treeRelation.treePath(),
+								treeRelation.treeNode(),
+								ldesConfig.getHostName() + "/" + ldesConfig.getCollectionName()
+										+ treeRelation.treeValue(),
+								treeRelation.treeValueType(), treeRelation.relation());
+						statements.addAll(treeRelationResponse.convertToStatements(ldesConfig.getHostName() + "/"
+								+ ldesConfig.getCollectionName() + view.getFragmentId()));
+					});
+
 		});
 
 		return statements;
--- ldes-server-port-fetch-rest/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/rest/eventstream/EventStreamControllerTest.java
+++ ldes-server-port-fetch-rest/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/rest/eventstream/EventStreamControllerTest.java
@@ -10,7 +10,6 @@ import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.se
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.services.EventStreamFactory;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.valueobjects.EventStream;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.node.entities.TreeNode;
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.relations.services.RelationStatementConverterImpl;
 import be.vlaanderen.informatievlaanderen.ldes.server.rest.caching.CachingStrategy;
 import be.vlaanderen.informatievlaanderen.ldes.server.rest.caching.EtagCachingStrategy;
 import be.vlaanderen.informatievlaanderen.ldes.server.rest.config.RestConfig;
@@ -155,8 +154,7 @@ class EventStreamControllerTest {
 		@Bean
 		public EventStreamConverter eventStreamConverter(final LdesConfig ldesConfig) {
 			PrefixAdder prefixAdder = new PrefixAdderImpl();
-			return new EventStreamConverterImpl(prefixAdder, ldesConfig,
-					new RelationStatementConverterImpl(ldesConfig));
+			return new EventStreamConverterImpl(prefixAdder, ldesConfig);
 		}
 
 		@Bean
