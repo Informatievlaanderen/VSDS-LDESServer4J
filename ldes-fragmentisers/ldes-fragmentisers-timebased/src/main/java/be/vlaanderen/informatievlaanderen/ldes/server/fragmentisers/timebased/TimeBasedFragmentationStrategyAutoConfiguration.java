bcc1db3c23c3426a405f54208c6fcae480e68e95
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-02-17 10:59:07 +0100

    Deprecate

--- ldes-fragmentisers/ldes-fragmentisers-timebased/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/timebased/TimeBasedFragmentationStrategyAutoConfiguration.java
+++ ldes-fragmentisers/ldes-fragmentisers-timebased/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/timebased/TimeBasedFragmentationStrategyAutoConfiguration.java
@@ -1,5 +1,8 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.timebased;
 
+import be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.timebased.services.TimeBasedFragmentCreator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,8 +13,11 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("be.vlaanderen.informatievlaanderen.ldes.server")
 public class TimeBasedFragmentationStrategyAutoConfiguration {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TimeBasedFragmentationStrategyAutoConfiguration.class);
+
 	@Bean("timebased")
 	public TimebasedFragmentationStrategyWrapper timebasedFragmentationStrategyWrapper() {
+		LOGGER.warn("You are still using timebased fragmentation. This is deprecated and no longer supported. ");
 		return new TimebasedFragmentationStrategyWrapper();
 	}
 }
