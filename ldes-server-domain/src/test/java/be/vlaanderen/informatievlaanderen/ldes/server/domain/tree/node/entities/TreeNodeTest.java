84c215042b520b21eba0989d5311335ea92b98d9
jonasbu <jonas.bulcke@cegeka.com>
2023-03-13 16:54:47 +0100

    fix: TreeNodeTest formatted

--- ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/tree/node/entities/TreeNodeTest.java
+++ ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/tree/node/entities/TreeNodeTest.java
@@ -13,35 +13,34 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TreeNodeTest {
-    private static final String ID = "FRAGMENT_ID";
+	private static final String ID = "FRAGMENT_ID";
 
-    @Test
-    void test_EqualityOfTreeNodes() {
-        var treeNode = new TreeNode(ID, false, false, true, List.of(), List.of());
-        var treeNode2 = new TreeNode(ID, true, false, true, null, List.of());
+	@Test
+	void test_EqualityOfTreeNodes() {
+		var treeNode = new TreeNode(ID, false, false, true, List.of(), List.of());
+		var treeNode2 = new TreeNode(ID, true, false, true, null, List.of());
 
-        assertEquals(treeNode, treeNode2);
-        assertEquals(treeNode, treeNode);
-        assertEquals(treeNode2, treeNode2);
-    }
+		assertEquals(treeNode, treeNode2);
+		assertEquals(treeNode, treeNode);
+		assertEquals(treeNode2, treeNode2);
+	}
 
-    @ParameterizedTest
-    @ArgumentsSource(TreeNodeArgumentProvider.class)
-    void test_InequalityOfTreeNodes(Object otherTreeNode) {
-        var treeNode = new TreeNode(ID, false, false, false, null, List.of());
+	@ParameterizedTest
+	@ArgumentsSource(TreeNodeArgumentProvider.class)
+	void test_InequalityOfTreeNodes(Object otherTreeNode) {
+		var treeNode = new TreeNode(ID, false, false, false, null, List.of());
 
-        assertNotEquals(treeNode, otherTreeNode);
+		assertNotEquals(treeNode, otherTreeNode);
 
-    }
+	}
 
-    static class TreeNodeArgumentProvider implements ArgumentsProvider {
-        @Override
-        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
-            return Stream.of(
-                    Arguments.of((Object) null),
-                    Arguments.of(new TreeNode("Other id", true, true, false, List.of(), null)),
-                    Arguments.of(new TreeNode("Another id", false, false, false, null, List.of()))
-            );
-        }
-    }
+	static class TreeNodeArgumentProvider implements ArgumentsProvider {
+		@Override
+		public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+			return Stream.of(
+					Arguments.of((Object) null),
+					Arguments.of(new TreeNode("Other id", true, true, false, List.of(), null)),
+					Arguments.of(new TreeNode("Another id", false, false, false, null, List.of())));
+		}
+	}
 }
