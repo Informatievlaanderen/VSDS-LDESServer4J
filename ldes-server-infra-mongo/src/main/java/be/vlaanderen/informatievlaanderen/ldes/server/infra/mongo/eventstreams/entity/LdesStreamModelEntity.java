73cc72d0761612e0301a36d0f5853f88f543d20e
jonasbu <jonas.bulcke@cegeka.com>
2023-03-16 15:50:57 +0100

    feat: refactoring and adding LdesStreamModel + endpoints

---
+++ ldes-server-infra-mongo/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/eventstreams/entity/LdesStreamModelEntity.java
@@ -0,0 +1,41 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.eventstreams.entity;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.valueobjects.LdesStreamModel;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entities.Member;
+import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.member.entity.LdesMemberEntity;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFParserBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.StringWriter;
+
+@Document("ldesstreammodel")
+public class LdesStreamModelEntity {
+	@Id
+	private final String id;
+
+	private final String model;
+
+	public LdesStreamModelEntity(String id, String model) {
+		this.id = id;
+		this.model = model;
+	}
+
+	public static LdesStreamModelEntity fromLdesStreamModel(LdesStreamModel ldesStreamModel) {
+		StringWriter outputStream = new StringWriter();
+		RDFDataMgr.write(outputStream, ldesStreamModel.getModel(), Lang.NQUADS);
+		String ldesMemberString = outputStream.toString();
+		return new LdesStreamModelEntity(
+				ldesStreamModel.getId(),
+				ldesMemberString);
+	}
+
+	public LdesStreamModel toLdesStreamModel() {
+		Model ldesMemberModel = RDFParserBuilder.create().fromString(this.model).lang(Lang.NQUADS).toModel();
+		return new LdesStreamModel(this.id, ldesMemberModel);
+	}
+
+}
---
+++ ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/converters/LdesStreamModelConverter.java
@@ -0,0 +1,62 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.admin.rest.converters;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.valueobjects.LdesStreamModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+import static be.vlaanderen.informatievlaanderen.ldes.server.domain.constants.RdfConstants.RDF_SYNTAX_TYPE;
+import static be.vlaanderen.informatievlaanderen.ldes.server.domain.converter.RdfModelConverter.fromString;
+import static be.vlaanderen.informatievlaanderen.ldes.server.domain.converter.RdfModelConverter.getLang;
+import static be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.RdfFormatException.LdesProcessDirection.INGEST;
+import static org.apache.jena.rdf.model.ResourceFactory.createResource;
+import static org.apache.jena.riot.RDFFormat.NQUADS;
+
+public class LdesStreamModelConverter extends AbstractHttpMessageConverter<LdesStreamModel> {
+
+	@Override
+	protected boolean supports(Class<?> clazz) {
+		return clazz.isAssignableFrom(LdesStreamModel.class);
+	}
+
+	@Override
+	protected LdesStreamModel readInternal(Class<? extends LdesStreamModel> clazz, HttpInputMessage inputMessage)
+			throws IOException, HttpMessageNotReadableException {
+		Lang lang = getLang(Objects.requireNonNull(inputMessage.getHeaders().getContentType()), INGEST);
+		Model memberModel = fromString(new String(inputMessage.getBody().readAllBytes(), StandardCharsets.UTF_8), lang);
+		String memberId = extractStreamId(memberModel);
+		return new LdesStreamModel(memberId, memberModel);
+	}
+
+	private String extractStreamId(Model model) {
+		final String COLLECTION_NAME = "collectionName";
+		return model
+				.listStatements(null, RDF_SYNTAX_TYPE, createResource(COLLECTION_NAME))
+				.nextOptional()
+				.map(statement -> statement.getSubject().toString()).get();
+	}
+
+	@Override
+	protected void writeInternal(LdesStreamModel ldesStreamModel, HttpOutputMessage outputMessage)
+			throws IOException, HttpMessageNotWritableException {
+		Model fragmentModel = ldesStreamModel.getModel();
+
+		StringWriter outputStream = new StringWriter();
+
+		RDFDataMgr.write(outputStream, fragmentModel, NQUADS);
+
+		OutputStream body = outputMessage.getBody();
+		body.write(outputStream.toString().getBytes());
+	}
+}
