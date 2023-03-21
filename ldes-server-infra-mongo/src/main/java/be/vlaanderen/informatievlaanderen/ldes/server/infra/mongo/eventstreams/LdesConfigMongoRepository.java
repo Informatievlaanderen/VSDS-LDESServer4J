95971fa3a993cf7943161d7a68d8ad938cf1939e
jonasbu <jonas.bulcke@cegeka.com>
2023-03-21 15:27:48 +0100

    feat: already few sonar fixes added

--- ldes-server-infra-mongo/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/eventstreams/LdesConfigMongoRepository.java
+++ ldes-server-infra-mongo/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/eventstreams/LdesConfigMongoRepository.java
@@ -1,22 +1,18 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.eventstreams;
 
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.MissingLdesStreamException;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.repository.LdesConfigRepository;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.valueobjects.LdesConfigModel;
 import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.eventstreams.entity.LdesConfigModelEntity;
 import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.eventstreams.repository.LdesConfigEntityRepository;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.List;
 import java.util.Optional;
 
 public class LdesConfigMongoRepository implements LdesConfigRepository {
 	private final LdesConfigEntityRepository repository;
-	private final MongoTemplate mongoTemplate;
 
-	public LdesConfigMongoRepository(LdesConfigEntityRepository repository, MongoTemplate mongoTemplate) {
+	public LdesConfigMongoRepository(LdesConfigEntityRepository repository) {
 		this.repository = repository;
-		this.mongoTemplate = mongoTemplate;
 	}
 
 	@Override
@@ -32,10 +28,6 @@ public class LdesConfigMongoRepository implements LdesConfigRepository {
 		return repository
 				.findAllById(collection)
 				.map(LdesConfigModelEntity::toLdesStreamModel);
-	}
-
-	private LdesConfigModelEntity getLdesStreamEntity(String collection) {
-		return repository.findAllById(collection).orElseThrow(() -> new MissingLdesStreamException(collection));
 	}
 
 	@Override
