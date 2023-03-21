3b1ec534eec29afeb4da5f6a2c9a5ef7eef66d8d
jonasbu <jonas.bulcke@cegeka.com>
2023-03-21 11:41:41 +0100

    feat: first tests for LdesStreamShaclValidator

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/validation/LdesStreamShaclValidator.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/validation/LdesStreamShaclValidator.java
@@ -1,13 +1,8 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.domain.validation;
 
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.converter.RdfModelConverter;
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.LdesStreamShaclValidationException;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.valueobjects.LdesConfigModel;
-import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.shacl.ShaclValidator;
 import org.apache.jena.shacl.Shapes;
-import org.apache.jena.shacl.ValidationReport;
 import org.springframework.validation.Errors;
 
 public class LdesStreamShaclValidator extends AbstractShaclValidator {
@@ -25,15 +20,7 @@ public class LdesStreamShaclValidator extends AbstractShaclValidator {
 	@Override
 	public void validate(Object target, Errors errors) {
 		LdesConfigModel ldesConfigModel = (LdesConfigModel) target;
-		validateShape(ldesConfigModel);
-	}
-
-	protected void validateShape(LdesConfigModel ldesConfigModel) {
-		ValidationReport report = ShaclValidator.get().validate(shapes, ldesConfigModel.getModel().getGraph());
-
-		if (!report.conforms()) {
-			throw new LdesStreamShaclValidationException(RdfModelConverter.toString(report.getModel(), Lang.TURTLE));
-		}
+		validateShape(ldesConfigModel.getModel());
 	}
 
 	@Override
--- ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/config/AdminWebConfig.java
+++ ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/config/AdminWebConfig.java
@@ -5,6 +5,8 @@ import be.vlaanderen.informatievlaanderen.ldes.server.domain.validation.LdesStre
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.config.LdesAdminConstants.SHAPE;
+
 @Configuration
 public class AdminWebConfig {
 	@Bean
@@ -16,13 +18,11 @@ public class AdminWebConfig {
 	public LdesStreamShaclValidator ldesStreamShaclValidator() {
 		// shape needs to be retrieved from the config, needs to be the shacl shape of
 		// the stream
-		final String SHAPE = "";
 		return new LdesStreamShaclValidator(SHAPE);
 	}
 
 	@Bean(name = "viewShaclValidator")
 	public LdesStreamShaclValidator ldesViewShaclValidator() {
-		final String SHAPE = "";
 		return new LdesStreamShaclValidator(SHAPE);
 	}
 }
