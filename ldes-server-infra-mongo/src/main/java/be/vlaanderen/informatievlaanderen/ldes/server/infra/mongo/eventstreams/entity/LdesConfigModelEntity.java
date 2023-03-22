c700868167369d993118605f10da58e72041e93d
jonasbu <jonas.bulcke@cegeka.com>
2023-03-22 13:04:30 +0100

    feat: all tests added for LdesConfigModelServiceImpl

--- ldes-server-infra-mongo/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/eventstreams/entity/LdesConfigModelEntity.java
+++ ldes-server-infra-mongo/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/eventstreams/entity/LdesConfigModelEntity.java
@@ -3,12 +3,10 @@ package be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.eventstreams.
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldes.eventstream.valueobjects.LdesConfigModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFParserBuilder;
+import org.apache.jena.riot.RDFWriter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.io.StringWriter;
 
 @Document("ldesstreammodel")
 public class LdesConfigModelEntity {
@@ -23,9 +21,9 @@ public class LdesConfigModelEntity {
 	}
 
 	public static LdesConfigModelEntity fromLdesStreamModel(LdesConfigModel ldesConfigModel) {
-		StringWriter outputStream = new StringWriter();
-		RDFDataMgr.write(outputStream, ldesConfigModel.getModel(), Lang.TURTLE);
-		String ldesMemberString = outputStream.toString();
+		String ldesMemberString = RDFWriter.source(ldesConfigModel.getModel())
+				.lang(Lang.TURTLE)
+				.asString();
 		return new LdesConfigModelEntity(
 				ldesConfigModel.getId(),
 				ldesMemberString);
