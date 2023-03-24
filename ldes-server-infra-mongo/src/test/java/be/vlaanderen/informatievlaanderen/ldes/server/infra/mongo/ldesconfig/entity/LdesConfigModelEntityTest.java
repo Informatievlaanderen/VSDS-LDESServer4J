fd540b666ff33d6608a8b9867a03a0272933135d
pj-cegeka <pieterjan.livens@cegeka.com>
2023-03-24 09:34:07 +0100

    feat/ entity test

--- ldes-server-infra-mongo/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/ldesconfig/entity/LdesConfigModelEntityTest.java
+++ ldes-server-infra-mongo/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/ldesconfig/entity/LdesConfigModelEntityTest.java
@@ -16,33 +16,32 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.ldesconfig.entity.LdesConfigModelEntity.fromLdesConfigtModel;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 class LdesConfigModelEntityTest {
 
-    @Test
-    void when_LdesConfigModelConvertedAndBack_Then_ModelIsIsomorphic() throws URISyntaxException {
-        String fileName = "ldes-multiple-views.ttl";
-        Model model = readModelFromFile(fileName);
-        LdesConfigModel configModel = new LdesConfigModel("stream", model);
-        LdesConfigModelEntity entity = fromLdesConfigtModel(configModel);
-        LdesConfigModel actualConfigModel = entity.toLdesConfigModel();
-        Assertions.assertTrue(actualConfigModel.getModel().isIsomorphicWith(model));
-    }
+	@Test
+	void when_LdesConfigModelConvertedAndBack_Then_ModelIsIsomorphic() throws URISyntaxException {
+		String fileName = "ldes-multiple-views.ttl";
+		Model model = readModelFromFile(fileName);
+		LdesConfigModel configModel = new LdesConfigModel("stream", model);
+		LdesConfigModelEntity entity = fromLdesConfigtModel(configModel);
+		LdesConfigModel actualConfigModel = entity.toLdesConfigModel();
+		Assertions.assertTrue(actualConfigModel.getModel().isIsomorphicWith(model));
+	}
 
-    private String readDataFromFile(String fileName, Lang rdfFormat)
-            throws URISyntaxException, IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).toURI());
-        String content = Files.lines(Paths.get(file.toURI())).collect(Collectors.joining("\n"));
-        return content;
-    }
+	private String readDataFromFile(String fileName, Lang rdfFormat)
+			throws URISyntaxException, IOException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).toURI());
+		String content = Files.lines(Paths.get(file.toURI())).collect(Collectors.joining("\n"));
+		return content;
+	}
 
-    private Model readModelFromFile(String fileName) throws URISyntaxException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        String uri = Objects.requireNonNull(classLoader.getResource(fileName)).toURI()
-                .toString();
-        return RDFDataMgr.loadModel(uri);
-    }
+	private Model readModelFromFile(String fileName) throws URISyntaxException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		String uri = Objects.requireNonNull(classLoader.getResource(fileName)).toURI()
+				.toString();
+		return RDFDataMgr.loadModel(uri);
+	}
 }
