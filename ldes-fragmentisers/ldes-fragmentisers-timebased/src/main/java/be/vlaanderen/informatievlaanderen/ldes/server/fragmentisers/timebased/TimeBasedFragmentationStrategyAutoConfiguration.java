aaf8117183b10ddbcdda22727677989fb29b0cf5
WLefever <wouter.lefever@cegeka.com>
2023-04-05 10:29:48 +0200

    Revert "Feat/deprecate timebased fragmentation (#437)"
    This reverts commit ac03c2645b9be911ba73c9583a6a77184a2187fa.

--- ldes-fragmentisers/ldes-fragmentisers-timebased/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/timebased/TimeBasedFragmentationStrategyAutoConfiguration.java
+++ ldes-fragmentisers/ldes-fragmentisers-timebased/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentisers/timebased/TimeBasedFragmentationStrategyAutoConfiguration.java
@@ -1,7 +1,5 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.fragmentisers.timebased;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,12 +10,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("be.vlaanderen.informatievlaanderen.ldes.server")
 public class TimeBasedFragmentationStrategyAutoConfiguration {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(TimeBasedFragmentationStrategyAutoConfiguration.class);
-
 	@Bean("timebased")
 	public TimebasedFragmentationStrategyWrapper timebasedFragmentationStrategyWrapper() {
-		LOGGER.warn("Using deprecated timebased fragmentation. For more information, refer to " +
-				"https://github.com/Informatievlaanderen/VSDS-LDESServer4J/blob/main/ldes-fragmentisers/ldes-fragmentisers-timebased/README.MD");
 		return new TimebasedFragmentationStrategyWrapper();
 	}
 }
