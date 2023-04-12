493bbf0ac55e162e0cf8a155b63438ccf6ad37f1
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-04-12 17:10:03 +0200

    Foormat

--- ldes-server-port-ingestion-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingestion/rest/exceptionhandling/CollectionNotFoundException.java
+++ ldes-server-port-ingestion-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingestion/rest/exceptionhandling/CollectionNotFoundException.java
@@ -1,7 +1,7 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.ingestion.rest.exceptionhandling;
 
 public class CollectionNotFoundException extends RuntimeException {
-    public CollectionNotFoundException(String collectionName) {
-        super("Collection with name %s could not be found.".formatted(collectionName));
-    }
+	public CollectionNotFoundException(String collectionName) {
+		super("Collection with name %s could not be found.".formatted(collectionName));
+	}
 }
