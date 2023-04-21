1d930ec69616fd8d9c8bfedd97be254cc013e8c1
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-04-21 15:49:55 +0200

    Only add testing

--- ldes-server-infra-mongo/mongock-changeset-4/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/mongock/changeset4/MemberUpdaterChange.java
+++ ldes-server-infra-mongo/mongock-changeset-4/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/mongock/changeset4/MemberUpdaterChange.java
@@ -1,30 +1,20 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset4;
 
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.AppConfig;
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.LdesConfig;
 import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset4.entities.LdesMemberEntityV4;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.impl.LiteralImpl;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFParserBuilder;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
-
-@ChangeUnit(id = "member-updater-changeset-2", order = "2", author = "VSDS")
+@ChangeUnit(id = "member-updater-changeset-4", order = "4", author = "VSDS")
 public class MemberUpdaterChange {
 
 	private final MongoTemplate mongoTemplate;
 
-	public MemberUpdaterChange(MongoTemplate mongoTemplate, AppConfig appConfig) {
+	public MemberUpdaterChange(MongoTemplate mongoTemplate) {
 		this.mongoTemplate = mongoTemplate;
 	}
 
@@ -49,10 +39,18 @@ public class MemberUpdaterChange {
 	@RollbackExecution
 	public void rollback() {
 		List<LdesMemberEntityV4> ldesMemberEntities = mongoTemplate.find(new Query(), LdesMemberEntityV4.class);
-
-		ldesMemberEntities.forEach(ldesMember -> mongoTemplate
-				.save(new LdesMemberEntityV4(ldesMember.getId(), ldesMember.getModel(),
-						ldesMember.getTreeNodeReferences())));
+		ldesMemberEntities.forEach(ldesMember -> {
+			if (!ldesMember.getId().startsWith("http")) {
+				final String idWithoutPrefix = ldesMember.getId().substring(ldesMember.getId().indexOf("/") + 1);
+				final LdesMemberEntityV4 updatedMember = new LdesMemberEntityV4(idWithoutPrefix,
+						ldesMember.getCollectionName(), ldesMember.getSequenceNr(),
+						ldesMember.getVersionOf(), ldesMember.getTimestamp(), ldesMember.getModel(),
+						ldesMember.getTreeNodeReferences());
+				// we cannot change the id so we need to insert + delete
+				mongoTemplate.save(updatedMember);
+				mongoTemplate.remove(ldesMember);
+			}
+		});
 	}
 
 }
---
+++ ldes-server-infra-mongo/mongock-changeset-4/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/mongock/changeset4/MemberUpdaterChangeTest.java
@@ -0,0 +1,19 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset4;
+
+import org.junit.jupiter.api.Test;
+
+class MemberUpdaterChangeTest {
+
+
+
+    @Test
+    void changeSet() {
+
+    }
+
+    @Test
+    void rollback() {
+
+    }
+
+}
