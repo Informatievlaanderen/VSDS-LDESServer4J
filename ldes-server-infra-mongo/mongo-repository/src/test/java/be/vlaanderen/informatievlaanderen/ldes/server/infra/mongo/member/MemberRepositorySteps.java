5b8d9e8b266e5b1eca12276d3458f0e0376605f4
Wouter Lefever <wouter.lefever@cegeka.com>
2023-04-27 16:38:48 +0200

    Feat: Introduction of cucumber in mongo module (#543)

---
+++ ldes-server-infra-mongo/mongo-repository/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/member/MemberRepositorySteps.java
@@ -0,0 +1,62 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.member;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entities.Member;
+import io.cucumber.java.DataTableType;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.jena.rdf.model.ModelFactory;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MemberRepositorySteps extends SpringIntegrationTest {
+
+	private List<Member> members;
+	private Optional<Member> retrievedMember;
+
+	@When("I save the members using the MemberRepository")
+	public void iSaveTheMembers() {
+		members.forEach(memberRepository::saveLdesMember);
+	}
+
+	@DataTableType(replaceWithEmptyString = "[blank]")
+	public Member memberEntryTransformer(Map<String, String> row) {
+		return new Member(
+				row.get("id"),
+				row.get("collectionName"),
+				row.get("sequenceNr").equals("") ? null : Long.parseLong(row.get("sequenceNr")),
+				row.get("versionOf"), LocalDateTime.now(),
+				ModelFactory.createDefaultModel(), List.of());
+	}
+
+	@Given("The following members")
+	public void theFollowingMembers(List<Member> members) {
+		this.members = members;
+	}
+
+	@Then("The member with id {string} can be retrieved from the database")
+	public void theMemberWithIdCanBeRetrievedFromTheDatabase(String id) {
+		retrievedMember = memberRepository.getMember(id);
+	}
+
+	@And("The retrieved member has the same properties as the {int} member in the table and has sequenceNr {int}")
+	public void theRetrievedMemberHasTheSamePropertiesAsTheMemberInTheTableAndHasSequenceNr(int index, int sequencNr) {
+		assertTrue(retrievedMember.isPresent());
+		Member retrievedMemberPresent = retrievedMember.get();
+		Member member = members.get(index - 1);
+		assertEquals(member.getLdesMemberId(), retrievedMemberPresent.getLdesMemberId());
+		assertEquals(member.getCollectionName(), retrievedMemberPresent.getCollectionName());
+		assertEquals(sequencNr, retrievedMemberPresent.getSequenceNr());
+		assertEquals(member.getVersionOf(), retrievedMemberPresent.getVersionOf());
+		assertThat(member.getTimestamp()).isEqualToIgnoringNanos(retrievedMemberPresent.getTimestamp());
+		assertEquals(member.getTreeNodeReferences(), retrievedMemberPresent.getTreeNodeReferences());
+	}
+}
