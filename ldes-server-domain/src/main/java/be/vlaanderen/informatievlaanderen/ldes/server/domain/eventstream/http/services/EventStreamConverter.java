adb6cef48cc9316ead7dbc4f865ea13473f45ee7
jobulcke <jonas.bulcke@cegeka.com>
2023-04-28 18:17:39 +0200

    feat: EventStream completely migrated from LdesConfigModel

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/eventstream/http/services/EventStreamConverter.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/eventstream/http/services/EventStreamConverter.java
@@ -1,20 +1,17 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.http.services;
 
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.http.valueobjects.EventStreamHttpMessage;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.http.valueobjects.EventStreamResponse;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.valueobjects.EventStream;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.shacl.entities.ShaclShape;
-import org.apache.jena.rdf.model.Model;
 
 public class EventStreamConverter {
-	public EventStream fromHttpMessage(EventStreamHttpMessage eventStreamHttpMessage) {
-		return new EventStream(eventStreamHttpMessage.getCollection(), eventStreamHttpMessage.getTimestampPath(), eventStreamHttpMessage.getVersionOfPath(), eventStreamHttpMessage.getViews());
+	public EventStream fromHttpMessage(EventStreamResponse eventStreamResponse) {
+		return new EventStream(eventStreamResponse.getCollection(), eventStreamResponse.getTimestampPath(),
+				eventStreamResponse.getVersionOfPath(), eventStreamResponse.getViews());
 	}
 
-	public EventStreamHttpMessage toHttpMessage(EventStream eventStream, Model shaclShape) {
-		return new EventStreamHttpMessage(eventStream.getCollection(), eventStream.getTimestampPath(), eventStream.getVersionOfPath(), eventStream.getViews(), shaclShape);
-	}
-
-	public EventStreamHttpMessage toHttpMessage(EventStream eventStream, ShaclShape shaclShape) {
-		return toHttpMessage(eventStream, shaclShape.getModel());
+	public EventStreamResponse toHttpMessage(EventStream eventStream, ShaclShape shaclShape) {
+		return new EventStreamResponse(eventStream.getCollection(), eventStream.getTimestampPath(),
+				eventStream.getVersionOfPath(), eventStream.getViews(), shaclShape.getModel());
 	}
 }
