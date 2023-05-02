ac5a0fba647efa6935f5a03e5a89825f25951ea1
jobulcke <jonas.bulcke@cegeka.com>
2023-05-02 13:59:41 +0200

    feat: deletion of views in EventStream

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/eventstream/http/services/EventStreamConverter.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/eventstream/http/services/EventStreamConverter.java
@@ -3,15 +3,19 @@ package be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.http.s
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.http.valueobjects.EventStreamResponse;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.valueobjects.EventStream;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.shacl.entities.ShaclShape;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.ViewSpecification;
+
+import java.util.List;
 
 public class EventStreamConverter {
 	public EventStream fromHttpMessage(EventStreamResponse eventStreamResponse) {
 		return new EventStream(eventStreamResponse.getCollection(), eventStreamResponse.getTimestampPath(),
-				eventStreamResponse.getVersionOfPath(), eventStreamResponse.getViews());
+				eventStreamResponse.getVersionOfPath());
 	}
 
-	public EventStreamResponse toHttpMessage(EventStream eventStream, ShaclShape shaclShape) {
+	public EventStreamResponse toHttpMessage(EventStream eventStream, List<ViewSpecification> views,
+			ShaclShape shaclShape) {
 		return new EventStreamResponse(eventStream.getCollection(), eventStream.getTimestampPath(),
-				eventStream.getVersionOfPath(), eventStream.getViews(), shaclShape.getModel());
+				eventStream.getVersionOfPath(), views, shaclShape.getModel());
 	}
 }
