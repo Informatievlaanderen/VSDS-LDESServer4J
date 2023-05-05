46d8b8bde78ac62e6d991c0a55e7756f0de7af3d
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-05-05 09:59:11 +0200

    Add validator testing

--- ldes-server-port-ingest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingest/validation/defaultimpl/DefaultMemberIngestValidator.java
+++ ldes-server-port-ingest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingest/validation/defaultimpl/DefaultMemberIngestValidator.java
@@ -18,7 +18,6 @@ public class DefaultMemberIngestValidator implements MemberIngestValidator {
 		this.validatorFactory = validatorFactory;
 	}
 
-	// TODO: 5/05/2023 add testing
 	@EventListener
 	public void handleShaclChangedEvent(ShaclChangedEvent event) {
 		final ShaclShape shacl = event.getShacl();
