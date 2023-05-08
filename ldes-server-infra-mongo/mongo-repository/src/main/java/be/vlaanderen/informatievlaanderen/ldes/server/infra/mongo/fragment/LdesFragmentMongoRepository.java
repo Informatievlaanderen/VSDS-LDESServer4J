3602f6976b8e9e674b3da49f59be2c31ff7afb04
WLefever <wouter.lefever@cegeka.com>
2023-05-08 09:54:23 +0200

    Update LdesFragmentRemoverImpl

--- ldes-server-infra-mongo/mongo-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/fragment/LdesFragmentMongoRepository.java
+++ ldes-server-infra-mongo/mongo-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/fragment/LdesFragmentMongoRepository.java
@@ -110,9 +110,4 @@ public class LdesFragmentMongoRepository implements LdesFragmentRepository {
 		repository.removeByViewName(viewName);
 	}
 
-	@Override
-	public void removeLdesFragmentsOfView(String viewName) {
-		repository.removeByViewName(viewName);
-	}
-
 }
