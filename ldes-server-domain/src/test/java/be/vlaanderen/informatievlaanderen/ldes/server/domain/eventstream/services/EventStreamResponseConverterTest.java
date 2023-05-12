9095e9933346cd0b9c1c5b7b1c133c2cfb3e9c8a
pj-cegeka <pieterjan.livens@cegeka.com>
2023-05-12 13:25:29 +0200

    fix/ add open api

--- ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/eventstream/services/EventStreamResponseConverterTest.java
+++ ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/eventstream/services/EventStreamResponseConverterTest.java
@@ -5,9 +5,7 @@ import be.vlaanderen.informatievlaanderen.ldes.server.domain.eventstream.http.va
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.view.service.ViewSpecificationConverter;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.*;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.riot.RDFWriter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
