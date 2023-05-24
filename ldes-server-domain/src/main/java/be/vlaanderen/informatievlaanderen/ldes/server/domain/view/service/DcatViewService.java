e6f8f918279f00fa6957b13bfd8fc1dbeacddfde
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-05-24 12:36:54 +0200

    Setup interface for dcat view svc

---
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/view/service/DcatViewService.java
@@ -0,0 +1,12 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.domain.view.service;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.ViewName;
+import org.apache.jena.rdf.model.Model;
+
+public interface DcatViewService {
+    void create(ViewName viewName, Model dcat);
+
+    void update(ViewName viewName, Model dcat);
+
+    void remove(ViewName viewName);
+}
---
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/view/service/DcatViewServiceImpl.java
@@ -0,0 +1,24 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.domain.view.service;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.ViewName;
+import org.apache.jena.rdf.model.Model;
+import org.springframework.stereotype.Service;
+
+// TODO: 24/05/2023 impl
+@Service
+public class DcatViewServiceImpl implements DcatViewService {
+    @Override
+    public void create(ViewName viewName, Model dcat) {
+
+    }
+
+    @Override
+    public void update(ViewName viewName, Model dcat) {
+
+    }
+
+    @Override
+    public void remove(ViewName viewName) {
+
+    }
+}
