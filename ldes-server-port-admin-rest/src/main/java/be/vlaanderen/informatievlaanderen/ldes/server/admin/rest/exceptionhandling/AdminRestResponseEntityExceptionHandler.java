40764ff59f59a80bf33c7af1226e4b1f3920c9f1
jobulcke <jonas.bulcke@cegeka.com>
2023-05-25 13:35:40 +0200

    feat: init ServerDcatController

--- ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/exceptionhandling/AdminRestResponseEntityExceptionHandler.java
+++ ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/exceptionhandling/AdminRestResponseEntityExceptionHandler.java
@@ -1,8 +1,6 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.admin.rest.exceptionhandling;
 
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.LdesShaclValidationException;
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.MissingEventStreamException;
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.MissingShaclShapeException;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.*;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.snapshot.exception.SnapshotCreationException;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.view.exception.MissingViewException;
 import org.apache.jena.riot.RiotException;
@@ -18,14 +16,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class AdminRestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(value = { MissingShaclShapeException.class,
-			MissingEventStreamException.class, MissingViewException.class })
-	protected ResponseEntity<Object> hanldeMissingLdesConfigException(
+			MissingEventStreamException.class, MissingViewException.class, MissingServerDcatException.class})
+	protected ResponseEntity<Object> handleMissingResourceException(
 			RuntimeException ex, WebRequest request) {
 		return handleException(ex, HttpStatus.NOT_FOUND, request);
 	}
 
-	@ExceptionHandler(value = { LdesShaclValidationException.class })
-	protected ResponseEntity<Object> handleLdesShacleValidationException(
+	@ExceptionHandler(value = { LdesShaclValidationException.class, DcatAlreadyConfiguredException.class })
+	protected ResponseEntity<Object> handleValidationException(
 			RuntimeException ex, WebRequest request) {
 		return handleException(ex, HttpStatus.BAD_REQUEST, request);
 	}
---
+++ ldes-server-port-admin-rest/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/controllers/AdminServerDcatControllerTest.java
@@ -0,0 +1,128 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.admin.rest.controllers;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.admin.rest.converters.ModelConverter;
+import be.vlaanderen.informatievlaanderen.ldes.server.admin.rest.exceptionhandling.AdminRestResponseEntityExceptionHandler;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.converter.PrefixAdderImpl;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.DcatAlreadyConfiguredException;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.MissingServerDcatException;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.serverdcat.entities.ServerDcat;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.serverdcat.services.ServerDcatService;
+import org.apache.commons.io.FileUtils;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFParser;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@ContextConfiguration(classes = {AdminServerDcatController.class, ModelConverter.class, PrefixAdderImpl.class, AdminRestResponseEntityExceptionHandler.class})
+@ExtendWith(MockitoExtension.class)
+class AdminServerDcatControllerTest {
+	private static final String ID = "id";
+	@MockBean
+	private ServerDcatService service;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void when_PostValidDcatModel_then_ReturnStatus200() throws Exception {
+		final Model model = readModelFromFile("dcat/valid-server-dcat.ttl");
+		when(service.createServerDcat(any())).thenReturn(new ServerDcat(ID, model));
+
+		mockMvc.perform(post("/admin/api/v1/dcat")
+						.contentType(Lang.TURTLE.getHeaderString())
+						.content(readDataFromFile("dcat/valid-server-dcat.ttl")))
+				.andExpect(status().isOk())
+				.andExpect(content().string(ID));
+
+		verify(service).createServerDcat(argThat(model::isIsomorphicWith));
+	}
+
+	@Test
+	void when_ServerAlreadyHasDcatConfigured_and_PostDcat_then_Return400() throws Exception {
+		when(service.createServerDcat(any())).thenThrow(DcatAlreadyConfiguredException.class);
+
+		mockMvc.perform(post("/admin/api/v1/dcat")
+						.contentType(Lang.TURTLE.getHeaderString())
+						.content(readDataFromFile("dcat/valid-server-dcat.ttl")))
+				.andExpect(status().isBadRequest());
+
+		verify(service).createServerDcat(any());
+	}
+
+	@Test
+	void when_PutValidDcat_then_ReturnStatus200() throws Exception {
+		final Model model = readModelFromFile("dcat/valid-server-dcat.ttl");
+		when(service.updateServerDcat(eq(ID), any())).thenReturn(new ServerDcat(ID, model));
+
+		mockMvc.perform(put("/admin/api/v1/dcat/{id}", ID)
+						.contentType(Lang.TURTLE.getHeaderString())
+						.content(readDataFromFile("dcat/valid-server-dcat.ttl")))
+				.andExpect(status().isOk())
+				.andExpect(content().string(ID));
+
+		verify(service).updateServerDcat(eq(ID), argThat(model::isIsomorphicWith));
+	}
+
+	@Test
+	void when_ServerHasNoDcatYet_and_PutServerDcat_then_ReturnStatus404() throws Exception {
+		final String id = "id";
+		final Model model = readModelFromFile("dcat/valid-server-dcat.ttl");
+		when(service.updateServerDcat(eq(id), any())).thenThrow(MissingServerDcatException.class);
+
+		mockMvc.perform(put("/admin/api/v1/dcat/{id}", id)
+						.contentType(Lang.TURTLE.getHeaderString())
+						.content(readDataFromFile("dcat/valid-server-dcat.ttl")))
+				.andExpect(status().isNotFound());
+
+		verify(service).updateServerDcat(eq(id), argThat(model::isIsomorphicWith));
+	}
+
+	@Test
+	void when_DeleteExistingDcat_then_ReturnStatus200() throws Exception {
+		final String id = "id";
+
+		mockMvc.perform(delete("/admin/api/v1/dcat/{id}", id)).andExpect(status().isOk());
+
+		verify(service).deleteServerDcat(id);
+	}
+
+	@Test
+	void when_DeleteNonExistingDcat_then_ReturnStatus404() throws Exception {
+		final String id = "id";
+		doThrow(MissingServerDcatException.class).when(service).deleteServerDcat(id);
+
+		mockMvc.perform(delete("/admin/api/v1/dcat/{id}", id)).andExpect(status().isNotFound());
+
+		verify(service).deleteServerDcat(id);
+	}
+
+	private Model readModelFromFile(String fileName) throws IOException {
+		final Path filePath = ResourceUtils.getFile("classpath:" + fileName).toPath();
+		return RDFParser.source(filePath).lang(Lang.TURTLE).toModel();
+	}
+
+	private String readDataFromFile(String fileName) throws IOException {
+		final File file = ResourceUtils.getFile("classpath:" + fileName);
+		return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+	}
+}
