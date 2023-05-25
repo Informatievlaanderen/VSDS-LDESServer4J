aadece9fc94cdbab9c704c74ea05e3c155960c85
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-05-25 10:52:57 +0200

    Test service

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/view/service/DcatViewServiceImpl.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/view/service/DcatViewServiceImpl.java
@@ -1,8 +1,8 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.domain.view.service;
 
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.view.entity.DcatView;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.view.exception.MissingViewDcatException;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.view.repository.DcatViewRepository;
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.view.entity.DcatView;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.ViewName;
 import org.apache.jena.rdf.model.Model;
 import org.springframework.stereotype.Service;
@@ -21,7 +21,6 @@ public class DcatViewServiceImpl implements DcatViewService {
 		dcatViewRepository.save(DcatView.from(viewName, dcat));
 	}
 
-	// TODO TVB: 25/05/2023 test
 	@Override
 	public void update(ViewName viewName, Model dcat) {
 		if (dcatViewRepository.findByViewName(viewName).isEmpty()) {
