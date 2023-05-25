0f9d869df91b763e7b9752acb36b00c024410941
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-05-25 09:26:03 +0200

    Complete swagger

--- ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/controllers/AdminViewsDcatRestController.java
+++ ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/controllers/AdminViewsDcatRestController.java
@@ -50,32 +50,45 @@ public interface AdminViewsDcatRestController {
                                     							""")})
             }) Model dcat);
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400"),
+            @ApiResponse(responseCode = "404")
+    })
     @Operation(summary = "Update DCAT metadata for a view")
-    void updateDcat(String collectionName, String viewName,
-                    @Parameter(schema = @Schema(implementation = String.class), description = "a blank node of type dcat:DataService with only dcat:DataService properties "
-                            +
-                            "(e.g. dct:title) and relations (e.g. dct:license), excluding relations " +
-                            "(non-configurable metadata) to dcat:Catalog and dcat:DataService", example = """
-                            @prefix dct:   <http://purl.org/dc/terms/> .
-                            @prefix dcat:  <http://www.w3.org/ns/dcat#> .
-                            @prefix foaf:  <http://xmlns.com/foaf/0.1/> .
-                            @prefix org:   <http://www.w3.org/ns/org#> .
-                            @prefix legal: <http://www.w3.org/ns/legal#> .
-                            @prefix m8g:   <http://data.europa.eu/m8g/>
-                            @prefix locn:  <http://www.w3.org/ns/locn#>
+    void updateDcat(
+            @Parameter(description = "The name of the collection", example = "mobility-hindrances") String collectionName,
+            @Parameter(description = "The name of the view", example = "by-page") String viewName,
+            @RequestBody(description = "A blank node of type dcat:DataService with only dcat:DataService properties " +
+                    "(e.g. dct:title) and relations (e.g. dct:license), excluding relations " +
+                    "(non-configurable metadata) to dcat:Catalog and dcat:DataService", content = {
+                    @Content(mediaType = contentTypeTurtle, schema = @Schema(implementation = String.class), examples = {
+                            @ExampleObject(value = """
+                                    @prefix dct:   <http://purl.org/dc/terms/> .
+                                    @prefix dcat:  <http://www.w3.org/ns/dcat#> .
 
-                            [] a dcat:DataService ;
-                              dct:title "My geo-spatial view"@en ;
-                              dct:description "Geospatial fragmentation for my LDES"@en ;
-                              dct:license [
-                                a dct:LicenseDocument ;
-                                dct:type [
-                                  rdf:type skos:Concept;
-                                  skos:prefLabel "some license"@en
-                                ]
-                              ] .
-                            """) Model dcat);
+                                    [] a dcat:DataService ;
+                                      dct:title "My geo-spatial view"@en ;
+                                      dct:description "Geospatial fragmentation for my LDES"@en .
+                                    """)}),
+                    @Content(mediaType = contentTypeNQuads, schema = @Schema(implementation = String.class), examples = {
+                            @ExampleObject(value = """
+                                    _:genid1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/dcat#DataService> .
+                                    _:genid1 <http://purl.org/dc/terms/title> "My geo-spatial view"@en .
+                                    _:genid1 <http://purl.org/dc/terms/description> "Geospatial fragmentation for my LDES"@en .
+                                    """)}),
+                    @Content(mediaType = contentTypeJSONLD, schema = @Schema(implementation = String.class), examples = {
+                            @ExampleObject(value = """
+                                    [{"@type":["http://www.w3.org/ns/dcat#DataService"],"http://purl.org/dc/terms/title":[{"@value":"My geo-spatial view","@language":"en"}],"http://purl.org/dc/terms/description":[{"@value":"Geospatial fragmentation for my LDES","@language":"en"}]},{"@id":"http://www.w3.org/ns/dcat#DataService"}]
+                                    							""")})
+            }) Model dcat);
 
-    void deleteDcat(String collectionName, String viewName);
+    @ApiResponses({
+            @ApiResponse(responseCode = "200")
+    })
+    @Operation(summary = "Delete DCAT metadata for a view")
+    void deleteDcat(@Parameter(description = "The name of the collection", example = "mobility-hindrances") String collectionName,
+                    @Parameter(description = "The name of the view", example = "by-page") String viewName);
+
 
 }
