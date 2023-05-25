8e67f370602a6563a82ef5d71f42ec0a19b8a743
Jan Robert <jan.robert@cegeka.com>
2023-05-25 13:06:18 +0200

    fix: broken openapi endpoint

--- ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/config/AdminWebConfig.java
+++ ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/config/AdminWebConfig.java
@@ -3,6 +3,7 @@ package be.vlaanderen.informatievlaanderen.ldes.server.admin.rest.config;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.validation.EventStreamValidator;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.validation.LdesConfigShaclValidator;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.validation.ShaclShapeValidator;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,4 +25,19 @@ public class AdminWebConfig {
 	public EventStreamValidator eventStreamValidator() {
 		return new EventStreamValidator();
 	}
+
+	@Bean
+	public GroupedOpenApi adminGroup() {
+		String[] packages = { "be.vlaanderen.informatievlaanderen.ldes.server.admin.rest" };
+		return GroupedOpenApi.builder().group("admin").packagesToScan(packages)
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi defaultGroup() {
+		String[] packages = { "be.vlaanderen.informatievlaanderen.ldes.server.admin.rest" };
+		return GroupedOpenApi.builder().group("base").packagesToExclude(packages)
+				.build();
+	}
+
 }
