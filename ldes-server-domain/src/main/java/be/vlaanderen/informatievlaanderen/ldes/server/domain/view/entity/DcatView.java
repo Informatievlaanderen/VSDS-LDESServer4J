c820a58b7075fe184c1d8eccea134e80190cbea0
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-05-26 10:59:15 +0200

    Implement fetch flow for dcat

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/view/entity/DcatView.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/view/entity/DcatView.java
@@ -1,13 +1,19 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.domain.view.entity;
 
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.ViewName;
-import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.RDF;
 
+import java.util.List;
 import java.util.Objects;
 
 import static org.apache.commons.lang3.Validate.notNull;
+import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
+import static org.apache.jena.rdf.model.ResourceFactory.createStatement;
 
 public class DcatView {
+
+	public static final Property DCAT_DATA_SERVICE = createProperty("http://www.w3.org/ns/dcat#DataService");
 
 	private final ViewName viewName;
 	private final Model dcat;
@@ -27,6 +33,15 @@ public class DcatView {
 
 	public Model getDcat() {
 		return dcat;
+	}
+
+	// TODO TVB: 26/05/2023 test
+	public List<Statement> getStatementsWithBase(Resource viewDescription) {
+		Resource dataServiceId = getDcat().listSubjectsWithProperty(RDF.type, DCAT_DATA_SERVICE).next().asResource();
+
+		return getDcat().listStatements(dataServiceId, null, (RDFNode) null)
+				.mapWith(stmnt -> createStatement(viewDescription, stmnt.getPredicate(), stmnt.getObject()))
+				.toList();
 	}
 
 	@Override
