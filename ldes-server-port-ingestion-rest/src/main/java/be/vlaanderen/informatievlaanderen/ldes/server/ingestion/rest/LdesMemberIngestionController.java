efef7d698d97b511aa95ebdcd712aa9766196929
jobulcke <jonas.bulcke@cegeka.com>
2023-06-02 15:41:23 +0200

    feat: open api docs provided

--- ldes-server-port-ingestion-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingestion/rest/LdesMemberIngestionController.java
+++ ldes-server-port-ingestion-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingestion/rest/LdesMemberIngestionController.java
@@ -5,18 +5,13 @@ import be.vlaanderen.informatievlaanderen.ldes.server.domain.shacl.services.Shac
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.entities.Member;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.tree.member.services.MemberIngestService;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.validation.LdesShaclValidator;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Tag(name = "Ingest")
-public class LdesMemberIngestionController {
+public class LdesMemberIngestionController implements OpenApiLdesMemberIngestionController {
 	private final MemberIngestService memberIngestService;
 	private final ShaclShapeService shapeService;
 
@@ -26,10 +21,10 @@ public class LdesMemberIngestionController {
 		this.shapeService = shapeService;
 	}
 
+	@Override
 	@PostMapping(value = "{collectionname}")
-	@Operation(summary = "Ingest version object to collection")
 	public void ingestLdesMember(
-			@Parameter(schema = @Schema(implementation = String.class), description = "A valid RDF model of an LDES member") @RequestBody Member member,
+			@RequestBody Member member,
 			@PathVariable("collectionname") String collectionName) {
 		validateMember(member, collectionName);
 		memberIngestService.addMember(member);
