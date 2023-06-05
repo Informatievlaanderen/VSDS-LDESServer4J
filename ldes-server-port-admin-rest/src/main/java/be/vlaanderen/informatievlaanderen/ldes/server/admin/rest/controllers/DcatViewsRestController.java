41b39e6d35420f6d9fd6bdd8fd4a44af7c4925ce
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-06-05 11:51:20 +0200

    Remove validator

--- ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/controllers/DcatViewsRestController.java
+++ ldes-server-port-admin-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/controllers/DcatViewsRestController.java
@@ -1,6 +1,6 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.admin.rest.controllers;
 
-import be.vlaanderen.informatievlaanderen.ldes.server.domain.validation.DcatViewValidator;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.validation.dcat.DcatDataServiceValidator;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.view.service.DcatViewService;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.ViewName;
 import org.apache.jena.rdf.model.Model;
@@ -21,16 +21,16 @@ public class DcatViewsRestController implements OpenApiDcatViewsController {
 
 	private final DcatViewService dcatViewService;
 
-	private final DcatViewValidator dcatViewValidator;
+	private final DcatDataServiceValidator dcatDataServiceValidator;
 
-	public DcatViewsRestController(DcatViewService dcatViewService, DcatViewValidator dcatViewValidator) {
+	public DcatViewsRestController(DcatViewService dcatViewService, DcatDataServiceValidator dcatDataServiceValidator) {
 		this.dcatViewService = dcatViewService;
-		this.dcatViewValidator = dcatViewValidator;
+		this.dcatDataServiceValidator = dcatDataServiceValidator;
 	}
 
 	@InitBinder
 	private void initBinder(WebDataBinder binder) {
-		binder.setValidator(dcatViewValidator);
+		binder.setValidator(dcatDataServiceValidator);
 	}
 
 	@PostMapping(consumes = { contentTypeJSONLD, contentTypeNQuads, contentTypeTurtle })
