ec23cbe1fe4eb00e9c81accc84aad5fe0b4565a3
Jan Robert <jan.robert@cegeka.com>
2023-06-07 11:57:16 +0200

    fix: formatting

--- ldes-server-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/Application.java
+++ ldes-server-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/Application.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
