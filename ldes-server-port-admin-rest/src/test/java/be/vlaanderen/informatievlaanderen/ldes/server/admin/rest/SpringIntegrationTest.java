ec9449fde9c29b5f522af7bc8f8d02222f9cc385
Tom Van Besien <tom.vanbesien@gmail.com>
2023-07-03 10:44:01 +0200

    Remove mvc test

--- ldes-server-port-admin-rest/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/SpringIntegrationTest.java
+++ ldes-server-port-admin-rest/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/SpringIntegrationTest.java
@@ -24,7 +24,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
 @SpringBootTest
 @AutoConfigureMockMvc
 @CucumberContextConfiguration
