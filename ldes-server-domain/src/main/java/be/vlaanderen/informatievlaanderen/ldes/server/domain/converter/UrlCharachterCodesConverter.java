ae2249518634c73d37d066d6a130b2c3725c0531
pj-cegeka <pieterjan.livens@cegeka.com>
2023-07-03 19:36:08 +0200

    fix/ fix codesmells

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/converter/UrlCharachterCodesConverter.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/converter/UrlCharachterCodesConverter.java
@@ -3,7 +3,6 @@ package be.vlaanderen.informatievlaanderen.ldes.server.domain.converter;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.valueobjects.LdesFragmentIdentifier;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragmentrequest.valueobjects.FragmentPair;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -12,6 +11,8 @@ import java.util.List;
 import static java.util.stream.Collectors.joining;
 
 public class UrlCharachterCodesConverter {
+	private UrlCharachterCodesConverter() {
+	}
 
 	public static String encode(String hostname, LdesFragmentIdentifier identifier) {
 		List<FragmentPair> pairs = identifier.getFragmentPairs();
@@ -20,13 +21,7 @@ public class UrlCharachterCodesConverter {
 			return viewString;
 		}
 		return pairs.stream()
-				.map(pair -> {
-					try {
-						return pair.fragmentKey() + "=" + encodeValue(pair.fragmentValue());
-					} catch (UnsupportedEncodingException e) {
-						throw new RuntimeException(e);
-					}
-				})
+				.map(pair -> pair.fragmentKey() + "=" + encodeValue(pair.fragmentValue()))
 				.collect(joining("&", viewString + "?", ""));
 	}
 
@@ -34,7 +29,7 @@ public class UrlCharachterCodesConverter {
 		return URLDecoder.decode(parameter, StandardCharsets.UTF_8);
 	}
 
-	private static String encodeValue(String value) throws UnsupportedEncodingException {
+	private static String encodeValue(String value) {
 		return URLEncoder.encode(value, StandardCharsets.UTF_8);
 	}
 }
