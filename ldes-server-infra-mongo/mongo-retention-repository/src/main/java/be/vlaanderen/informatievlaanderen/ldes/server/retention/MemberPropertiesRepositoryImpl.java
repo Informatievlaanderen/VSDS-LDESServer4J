8907aabec18b83d59a457704e5d4d68843bb7843
pj-cegeka <119848850+pj-cegeka@users.noreply.github.com>
2023-07-06 14:34:48 +0200

    Feat/allocated member handler (#803)
    * feat/ add AllocatedMemberHandler
    
    * feat/ add AllocatedMemberHandler
    
    * feat/ unit test allocateMember

--- ldes-server-infra-mongo/mongo-retention-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/retention/MemberPropertiesRepositoryImpl.java
+++ ldes-server-infra-mongo/mongo-retention-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/retention/MemberPropertiesRepositoryImpl.java
@@ -1,8 +1,14 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.retention;
 
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.ViewName;
 import be.vlaanderen.informatievlaanderen.ldes.server.retention.entities.MemberProperties;
+import be.vlaanderen.informatievlaanderen.ldes.server.retention.entities.MemberPropertiesEntity;
 import be.vlaanderen.informatievlaanderen.ldes.server.retention.mapper.MemberPropertiesEntityMapper;
 import be.vlaanderen.informatievlaanderen.ldes.server.retention.repositories.MemberPropertiesRepository;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -10,13 +16,16 @@ import java.util.Optional;
 @Component
 public class MemberPropertiesRepositoryImpl implements MemberPropertiesRepository {
 
+	public static final String VIEWS = "views";
 	private final MemberPropertiesEntityRepository memberPropertiesEntityRepository;
 	private final MemberPropertiesEntityMapper memberPropertiesEntityMapper;
+	private final MongoTemplate mongoTemplate;
 
 	public MemberPropertiesRepositoryImpl(MemberPropertiesEntityRepository memberPropertiesEntityRepository,
-			MemberPropertiesEntityMapper memberPropertiesEntityMapper) {
+			MemberPropertiesEntityMapper memberPropertiesEntityMapper, MongoTemplate mongoTemplate) {
 		this.memberPropertiesEntityRepository = memberPropertiesEntityRepository;
 		this.memberPropertiesEntityMapper = memberPropertiesEntityMapper;
+		this.mongoTemplate = mongoTemplate;
 	}
 
 	@Override
@@ -27,5 +36,14 @@ public class MemberPropertiesRepositoryImpl implements MemberPropertiesRepositor
 	@Override
 	public Optional<MemberProperties> retrieve(String id) {
 		return memberPropertiesEntityRepository.findById(id).map(memberPropertiesEntityMapper::toMember);
+	}
+
+	@Override
+	public synchronized void allocateMember(String memberId, ViewName viewName) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("_id").is(memberId));
+		Update update = new Update();
+		update.push(VIEWS, viewName.asString());
+		mongoTemplate.upsert(query, update, MemberPropertiesEntity.class);
 	}
 }
---
+++ ldes-server-retention/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/retention/services/AllocatedMemberHandlerTest.java
@@ -0,0 +1,32 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.retention.services;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.events.fragmentation.MemberAllocatedEvent;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.ViewName;
+import be.vlaanderen.informatievlaanderen.ldes.server.retention.repositories.MemberPropertiesRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class AllocatedMemberHandlerTest {
+	private final static String ID = "id";
+	private final static ViewName VIEW = new ViewName("collection", "view");
+	private final static MemberAllocatedEvent event = new MemberAllocatedEvent(ID, VIEW);
+	private AllocatedMemberHandler handler;
+	private MemberPropertiesRepository repo;
+
+	@BeforeEach
+	void setUp() {
+		repo = mock(MemberPropertiesRepository.class);
+		handler = new AllocatedMemberHandler(repo);
+	}
+
+	@Test
+	void allocateMember() {
+		handler.handleEventMemberIngestedEvent(event);
+
+		verify(repo).allocateMember(ID, VIEW);
+	}
+
+}
