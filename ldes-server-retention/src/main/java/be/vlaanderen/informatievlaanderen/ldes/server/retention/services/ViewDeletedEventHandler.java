9cc31b4154ec9fd2eb6d641ee65fceaba5937ec4
pj-cegeka <119848850+pj-cegeka@users.noreply.github.com>
2023-07-10 09:54:23 +0200

    Feat/handle view deleted (#811)
    * feat/ temp
    
    * feat/ remove member from view when view deleted
    
    * feat/ remove member from view when view deleted

---
+++ ldes-server-retention/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/retention/services/ViewDeletedEventHandler.java
@@ -0,0 +1,24 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.retention.services;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.view.valueobject.ViewDeletedEvent;
+import be.vlaanderen.informatievlaanderen.ldes.server.retention.repositories.MemberPropertiesRepository;
+import be.vlaanderen.informatievlaanderen.ldes.server.retention.services.retentionpolicy.execution.MemberRemover;
+import org.springframework.context.event.EventListener;
+
+public class ViewDeletedEventHandler {
+	private final MemberPropertiesRepository memberPropertiesRepository;
+	private final MemberRemover memberRemover;
+
+	public ViewDeletedEventHandler(MemberPropertiesRepository memberPropertiesRepository, MemberRemover memberRemover) {
+		this.memberPropertiesRepository = memberPropertiesRepository;
+		this.memberRemover = memberRemover;
+	}
+
+	// call from retentionPolicyCollectionImpl.handleViewDeletedEvent?
+	@EventListener
+	public void handleViewDeletedEvent(ViewDeletedEvent event) {
+		String viewName = event.getViewName().asString();
+		memberPropertiesRepository.getMemberPropertiesWithViewReference(viewName)
+				.forEach(memberProperties -> memberRemover.removeMemberFromView(memberProperties, viewName));
+	}
+}
