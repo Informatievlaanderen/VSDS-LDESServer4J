a3b106f6c8a410006cd83e84ca0dda8c2f77aeef
Tom Van Besien <tom.vanbesien@gmail.com>
2023-07-25 09:45:50 +0200

    test rootfragmentretriever

--- ldes-fragmentisers/ldes-fragmentisers-common/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentation/RootFragmentRetrieverTest.java
+++ ldes-fragmentisers/ldes-fragmentisers-common/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentation/RootFragmentRetrieverTest.java
@@ -1,11 +1,13 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.fragmentation;
 
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.MissingRootFragmentException;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.ldesfragment.valueobjects.LdesFragmentIdentifier;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.viewcreation.valueobjects.ViewName;
 import be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.entities.Fragment;
 import be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.repository.FragmentRepository;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -15,36 +17,55 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class RootFragmentRetrieverTest {
 
-    @Mock
-    private FragmentRepository fragmentRepository;
-    @Mock
-    private ObservationRegistry observationRegistry;
+	@Mock
+	private FragmentRepository fragmentRepository;
 
-    private static final ViewName VIEW_NAME = new ViewName("collection", "view");
+	private Observation observation;
+	private RootFragmentRetriever rootFragmentRetriever;
+	private Fragment rootFragment;
 
-    @Test
-    void should_FetchRootFragment_when_NotYetInMemory() {
-        final RootFragmentRetriever rootFragmentRetriever =
-                new RootFragmentRetriever(fragmentRepository, observationRegistry);
-        final Fragment rootFragment = new Fragment(new LdesFragmentIdentifier(VIEW_NAME, List.of()));
-        when(fragmentRepository.retrieveRootFragment(VIEW_NAME.asString()))
-                .thenReturn(Optional.of(rootFragment));
+	private static final ObservationRegistry observationRegistry = ObservationRegistry.NOOP;
+	private static final ViewName VIEW_NAME = new ViewName("collection", "view");
 
-        // TODO TVB: 24/07/23 fix
+	@BeforeEach
+	void setUp() {
+		observation = Observation.createNotStarted("observation", observationRegistry).start();
 
-        final Fragment result = rootFragmentRetriever.retrieveRootFragmentOfView(VIEW_NAME, mock(Observation.class));
+		rootFragmentRetriever = new RootFragmentRetriever(fragmentRepository, observationRegistry);
+		rootFragment = new Fragment(new LdesFragmentIdentifier(VIEW_NAME, List.of()));
+		when(fragmentRepository.retrieveRootFragment(VIEW_NAME.asString())).thenReturn(Optional.of(rootFragment));
+	}
 
-        assertEquals(rootFragment, result);
+	@Test
+	void should_FetchRootFragment_when_NotYetInMemory() {
+		final Fragment result = rootFragmentRetriever.retrieveRootFragmentOfView(VIEW_NAME, observation);
+
+		assertEquals(rootFragment, result);
+		verify(fragmentRepository).retrieveRootFragment(VIEW_NAME.asString());
+	}
+
+	@Test
+	void should_ReturnFragmentFromMemory_when_FetchedEarlier() {
+		rootFragmentRetriever.retrieveRootFragmentOfView(VIEW_NAME, observation);
+		rootFragmentRetriever.retrieveRootFragmentOfView(VIEW_NAME, observation);
+		rootFragmentRetriever.retrieveRootFragmentOfView(VIEW_NAME, observation);
+		final Fragment result = rootFragmentRetriever.retrieveRootFragmentOfView(VIEW_NAME, observation);
+
+		assertEquals(rootFragment, result);
+		verify(fragmentRepository, times(1)).retrieveRootFragment(VIEW_NAME.asString());
+	}
+
+	@Test
+    void should_ThrowException_when_RootFragmentIsNotFound() {
+        when(fragmentRepository.retrieveRootFragment(VIEW_NAME.asString())).thenReturn(Optional.empty());
+
+        assertThrows(MissingRootFragmentException.class,
+                () -> rootFragmentRetriever.retrieveRootFragmentOfView(VIEW_NAME, observation));
     }
-
-    @Test
-    void should_ReturnFragmentFromMemory_when_FetchedEarlier() {
-
-    }
-
 }
