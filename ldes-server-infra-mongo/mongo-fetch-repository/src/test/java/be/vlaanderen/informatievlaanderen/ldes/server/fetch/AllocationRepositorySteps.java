63206e803ff624b6956abf3d91c52cdfa4aabbcc
WLefever <wouter.lefever@cegeka.com>
2023-08-04 21:13:35 +0200

    feat: Refactor AllocationRepository

--- ldes-server-infra-mongo/mongo-fetch-repository/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fetch/AllocationRepositorySteps.java
+++ ldes-server-infra-mongo/mongo-fetch-repository/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fetch/AllocationRepositorySteps.java
@@ -16,82 +16,85 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AllocationRepositorySteps extends MongoAllocationIntegrationTest {
 
-    private List<MemberAllocation> memberAllocations;
+	private List<MemberAllocation> memberAllocations;
 
-    @DataTableType
-    public MemberAllocation memberAllocationEntryTransformer(Map<String, String> row) {
-        return new MemberAllocation(
-                row.get("id"),
-                row.get("collectionName"),
-                row.get("viewName"),
-                row.get("fragmentId"),
-                row.get("memberId"));
-    }
+	@DataTableType
+	public MemberAllocation memberAllocationEntryTransformer(Map<String, String> row) {
+		return new MemberAllocation(
+				row.get("id"),
+				row.get("collectionName"),
+				row.get("viewName"),
+				row.get("fragmentId"),
+				row.get("memberId"));
+	}
 
-    @DataTableType(replaceWithEmptyString = "[blank]")
-    public RetrievedMemberAllocations retrievedMemberAllocationsEntryTransformer(Map<String, String> row) {
-        return new RetrievedMemberAllocations(
-                row.get("fragmentId"),
-                row.get("ids").equals("") ? List.of() : Arrays.stream(row.get("ids").split(",")).sorted().toList());
-    }
+	@DataTableType(replaceWithEmptyString = "[blank]")
+	public RetrievedMemberAllocations retrievedMemberAllocationsEntryTransformer(Map<String, String> row) {
+		return new RetrievedMemberAllocations(
+				row.get("fragmentId"),
+				row.get("ids").equals("") ? List.of() : Arrays.stream(row.get("ids").split(",")).sorted().toList());
+	}
 
-    @Given("The following MemberAllocations")
-    public void theFollowingMemberAllocations(List<MemberAllocation> memberAllocations) {
-        this.memberAllocations = memberAllocations;
-    }
+	@Given("The following MemberAllocations")
+	public void theFollowingMemberAllocations(List<MemberAllocation> memberAllocations) {
+		this.memberAllocations = memberAllocations;
+	}
 
-    @And("They are ingested using the AllocationRepository")
-    public void theyAreIngestedUsingTheAllocationRepository() {
-        memberAllocations.forEach(allocationMongoRepository::saveAllocation);
-    }
+	@And("They are ingested using the AllocationRepository")
+	public void theyAreIngestedUsingTheAllocationRepository() {
+		memberAllocations.forEach(allocationMongoRepository::saveAllocation);
+	}
 
-    @Then("Querying by the fragment id has the following results")
-    public void queryingByTheFragmentIdHasTheFollowingResults(
-            List<RetrievedMemberAllocations> retrievedMemberAllocations) {
-        retrievedMemberAllocations.forEach(retrievedMemberAllocation -> assertMemberAllocationIdsEqualExpectedIds(retrievedMemberAllocation.fragmentId(), retrievedMemberAllocation.expectedIds()));
-    }
+	@Then("Querying by the fragment id has the following results")
+	public void queryingByTheFragmentIdHasTheFollowingResults(
+			List<RetrievedMemberAllocations> retrievedMemberAllocations) {
+		retrievedMemberAllocations.forEach(retrievedMemberAllocation -> assertMemberAllocationIdsEqualExpectedIds(
+				retrievedMemberAllocation.fragmentId(), retrievedMemberAllocation.expectedIds()));
+	}
 
-    @When("^Deleting by the member id ([^ ]+) and the collection name ([^ ]+) and the view name ([^ ]+)")
-    public void deletingByTheMemberIdMemberIdAndTheCollectionNameCollectionNameAndTheViewNameViewName(
-            String memberId, String collectionName, String viewName) {
-        allocationMongoRepository.deleteByMemberIdAndCollectionNameAndViewName(memberId, collectionName, viewName);
-    }
+	@When("^Deleting by the member id ([^ ]+) and the collection name ([^ ]+) and the view name ([^ ]+)")
+	public void deletingByTheMemberIdMemberIdAndTheCollectionNameCollectionNameAndTheViewNameViewName(
+			String memberId, String collectionName, String viewName) {
+		allocationMongoRepository.deleteByMemberIdAndCollectionNameAndViewName(memberId, collectionName, viewName);
+	}
 
-    @Then("^Querying by the fragment id ([^ ]+) has the following results ([^ ]+)")
-    public void queryingByTheFragmentIdFragmentIdHasTheFollowingResultsIds(String fragmentId, String results) {
-        List<String> expectedIds = results.equals("[blank]") ? List.of() : Arrays.stream(results.split(",")).sorted().toList();
-        assertMemberAllocationIdsEqualExpectedIds(fragmentId, expectedIds);
-    }
+	@Then("^Querying by the fragment id ([^ ]+) has the following results ([^ ]+)")
+	public void queryingByTheFragmentIdFragmentIdHasTheFollowingResultsIds(String fragmentId, String results) {
+		List<String> expectedIds = results.equals("[blank]") ? List.of()
+				: Arrays.stream(results.split(",")).sorted().toList();
+		assertMemberAllocationIdsEqualExpectedIds(fragmentId, expectedIds);
+	}
 
-    @When("^Deleting by the collection name ([^ ]+) and the view name ([^ ]+)")
-    public void deletingByTheCollectionNameCollectionNameAndTheViewNameViewName(String collectionName, String viewName) {
-        allocationMongoRepository.deleteByCollectionNameAndViewName(collectionName, viewName);
-    }
+	@When("^Deleting by the collection name ([^ ]+) and the view name ([^ ]+)")
+	public void deletingByTheCollectionNameCollectionNameAndTheViewNameViewName(String collectionName,
+			String viewName) {
+		allocationMongoRepository.deleteByCollectionNameAndViewName(collectionName, viewName);
+	}
 
-    @Then("^Querying by the fragment ids ([^ ]+) returns empty list")
-    public void queryingByTheFragmentIdsFragmentIdsReturnsEmptyList(String fragmentIds) {
-        Arrays.stream(fragmentIds.split(",")).forEach(fragmentId -> {
-            assertTrue(allocationMongoRepository.getMemberAllocationsByFragmentId(fragmentId).isEmpty());
-        });
-    }
+	@Then("^Querying by the fragment ids ([^ ]+) returns empty list")
+	public void queryingByTheFragmentIdsFragmentIdsReturnsEmptyList(String fragmentIds) {
+		Arrays.stream(fragmentIds.split(",")).forEach(fragmentId -> {
+			assertTrue(allocationMongoRepository.getMemberAllocationsByFragmentId(fragmentId).isEmpty());
+		});
+	}
 
-    private void assertMemberAllocationIdsEqualExpectedIds(String fragmentId, List<String> expectedIds) {
-        List<MemberAllocation> memberAllocationsByFragmentId = allocationMongoRepository
-                .getMemberAllocationsByFragmentId(fragmentId);
-        List<String> actualIds = memberAllocationsByFragmentId.stream().map(MemberAllocation::getId).sorted().toList();
-        assertEquals(expectedIds, actualIds);
-    }
+	private void assertMemberAllocationIdsEqualExpectedIds(String fragmentId, List<String> expectedIds) {
+		List<MemberAllocation> memberAllocationsByFragmentId = allocationMongoRepository
+				.getMemberAllocationsByFragmentId(fragmentId);
+		List<String> actualIds = memberAllocationsByFragmentId.stream().map(MemberAllocation::getId).sorted().toList();
+		assertEquals(expectedIds, actualIds);
+	}
 
-    @And("There are {int} remaining MemberAllocations in the MemberAllocationRepository")
-    public void thereAreRemainingMemberAllocationsInTheMemberAllocationRepository(int expectedCount) {
-        assertEquals(expectedCount, allocationEntityRepository.count());
-    }
+	@And("There are {int} remaining MemberAllocations in the MemberAllocationRepository")
+	public void thereAreRemainingMemberAllocationsInTheMemberAllocationRepository(int expectedCount) {
+		assertEquals(expectedCount, allocationEntityRepository.count());
+	}
 
-    @When("^Deleting by the collection name ([^ ]+)")
-    public void deletingByTheCollectionNameCollectionName(String collectionName) {
-        allocationMongoRepository.deleteByCollectionName(collectionName);
-    }
+	@When("^Deleting by the collection name ([^ ]+)")
+	public void deletingByTheCollectionNameCollectionName(String collectionName) {
+		allocationMongoRepository.deleteByCollectionName(collectionName);
+	}
 
-    private record RetrievedMemberAllocations(String fragmentId, List<String> expectedIds) {
-    }
+	private record RetrievedMemberAllocations(String fragmentId, List<String> expectedIds) {
+	}
 }
