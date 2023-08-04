945608f81d789529210e2f2790590752b98545d9
Tomvbe <34196062+Tomvbe@users.noreply.github.com>
2023-08-04 17:42:46 +0200

    Add missing annotation for eventListener to work (#870)

--- ldes-fragmentisers/ldes-fragmentisers-common/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentation/FragmentationSequenceListener.java
+++ ldes-fragmentisers/ldes-fragmentisers-common/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentation/FragmentationSequenceListener.java
@@ -3,7 +3,9 @@ package be.vlaanderen.informatievlaanderen.ldes.server.fragmentation;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.view.valueobject.ViewDeletedEvent;
 import be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.repository.FragmentSequenceRepository;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FragmentationSequenceListener {
 
 	private final FragmentSequenceRepository fragmentSequenceRepository;
