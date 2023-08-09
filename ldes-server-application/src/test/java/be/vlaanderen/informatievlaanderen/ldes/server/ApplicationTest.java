28a451437350432eb3bba67a2decf42cbc665d76
Wouter Lefever <wouter.lefever@cegeka.com>
2023-08-09 09:28:55 +0200

    feat: Restructure Fetching Module

---
+++ ldes-server-application/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/ApplicationTest.java
@@ -0,0 +1,25 @@
+package be.vlaanderen.informatievlaanderen.ldes.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    ApplicationModules modules = ApplicationModules.of(Application.class);
+
+    @Test
+    void verifiesModularStructure() {
+        modules.verify();
+    }
+
+    @Test
+    void createModuleDocumentation() {
+        new Documenter(modules)
+                .writeDocumentation()
+                .writeIndividualModulesAsPlantUml();
+    }
+
+}
