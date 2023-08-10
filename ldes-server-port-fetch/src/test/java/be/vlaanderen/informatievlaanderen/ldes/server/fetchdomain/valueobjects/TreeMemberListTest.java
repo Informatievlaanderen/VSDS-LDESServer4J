6b1dac00744b9d0eacadc6e4883d79bcd8b69c44
Wouter Lefever <wouter.lefever@cegeka.com>
2023-08-10 20:38:04 +0200

    update package names

--- ldes-server-port-fetch/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fetchdomain/valueobjects/TreeMemberListTest.java
+++ ldes-server-port-fetch/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fetchdomain/valueobjects/TreeMemberListTest.java
@@ -8,23 +8,27 @@ import org.apache.jena.riot.RDFParserBuilder;
 import org.junit.jupiter.api.Test;
 
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Objects;
 
-import static org.apache.jena.rdf.model.ResourceFactory.createResource;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class TreeMemberTest {
+class TreeMemberListTest {
 
 	@Test
-	void when_TreeMemberIsConvertedToStatements_then_ResultingModelIsAsExpected() throws URISyntaxException {
-		Model model = RDFParserBuilder.create().fromString("""
+	void when_TreeMemberListIsConvertedToStatements_then_ResultingModelIsAsExpected() throws URISyntaxException {
+		Model modelOne = RDFParserBuilder.create().fromString("""
 				<http://localhost:8080/people/1> <http://schema.org/name> "Jane Doe".""").lang(Lang.NQUADS).toModel();
-		TreeMember treeMember = new TreeMember("http://localhost:8080/people/1", model);
+		TreeMember treeMemberOne = new TreeMember("http://localhost:8080/people/1", modelOne);
+		Model modelTwo = RDFParserBuilder.create().fromString("""
+				<http://localhost:8080/people/2> <http://schema.org/name> "John Doe".""").lang(Lang.NQUADS).toModel();
+		TreeMember treeMemberTwo = new TreeMember("http://localhost:8080/people/2", modelTwo);
+		TreeMemberList treeMemberList = new TreeMemberList("http://localhost:8080/people",
+				List.of(treeMemberOne, treeMemberTwo));
 
-		Model actualModel = ModelFactory.createDefaultModel()
-				.add(treeMember.convertToStatements(createResource("http://localhost:8080/people")));
+		Model actualModel = ModelFactory.createDefaultModel().add(treeMemberList.convertToStatements());
 
-		Model expectedModel = readModelFromFile("valueobjects/treemember.ttl");
+		Model expectedModel = readModelFromFile("valueobjects/treememberlist.ttl");
 		assertTrue(actualModel.isIsomorphicWith(expectedModel));
 	}
 
--- ldes-server-port-fetch/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fetchdomain/valueobjects/TreeRelationTest.java
+++ ldes-server-port-fetch/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/fetchdomain/valueobjects/TreeRelationTest.java
@@ -2,14 +2,10 @@ package be.vlaanderen.informatievlaanderen.ldes.server.fetchdomain.valueobjects;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.junit.jupiter.api.Test;
 
-import java.io.StringWriter;
 import java.net.URISyntaxException;
-import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,7 +15,7 @@ class TreeRelationTest {
 	private static final String COLLECTION_NAME = "mobility-hindrances";
 
 	@Test
-	void when_RelationsAreNotEmpty_MultipleStatementsAreReturned() throws URISyntaxException {
+	void when_TreeRelationIsConvertedToStatements_then_ResultingModelIsAsExpected() throws URISyntaxException {
 		String treeNodeIdentifierOfRelation = HOST_NAME + "/" + COLLECTION_NAME + "/otherNode";
 		String treeNodeIdentifier = HOST_NAME + "/" + COLLECTION_NAME + "/node";
 		TreeRelation treeRelation = new TreeRelation("http://www.w3.org/ns/prov#generatedAtTime",
