395270acd37050ede39331d92fc90cf3a92baf1f
Wouter Lefever <wouter.lefever@cegeka.com>
2023-08-24 10:12:56 +0200

    IT

--- ldes-server-compaction/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/compaction/CompactionServiceSteps.java
+++ ldes-server-compaction/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/compaction/CompactionServiceSteps.java
@@ -1,26 +1,53 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.compaction;
 
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.events.admin.ViewAddedEvent;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.model.LdesFragmentIdentifier;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.model.TreeRelation;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.model.ViewName;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.model.ViewSpecification;
+import be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.entities.Fragment;
 import io.cucumber.java.DataTableType;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import static be.vlaanderen.informatievlaanderen.ldes.server.domain.constants.RdfConstants.GENERIC_TREE_RELATION;
+import static org.mockito.Mockito.when;
 
 public class CompactionServiceSteps extends CompactionIntegrationTest {
 
-	@DataTableType
-	public ViewSpecification ViewSpecificationEntryTransformer(Map<String, String> row) {
-		return new ViewSpecification(
-				ViewName.fromString(row.get("viewName")),
-				List.of(), List.of(), Integer.parseInt(row.get("pageSize")));
-	}
+    @DataTableType
+    public ViewSpecification ViewSpecificationEntryTransformer(Map<String, String> row) {
+        return new ViewSpecification(
+                ViewName.fromString(row.get("viewName")),
+                List.of(), List.of(), Integer.parseInt(row.get("pageSize")));
+    }
 
-	@Given("a view with the following properties")
-	public void aViewWithTheFollowingProperties(ViewSpecification viewSpecification) {
-		applicationEventPublisher.publishEvent(new ViewAddedEvent(viewSpecification));
-	}
+    @DataTableType(replaceWithEmptyString = "[blank]")
+    public Fragment FragmentEntryTransformer(Map<String, String> row) {
+        return new Fragment(
+                LdesFragmentIdentifier.fromFragmentId(row.get("fragmentIdentifier")),
+                Boolean.parseBoolean(row.get("immutable")), 0, row.get("relation").equals("") ? List.of() : List.of(new TreeRelation("", LdesFragmentIdentifier.fromFragmentId(row.get("relation")), "", "", GENERIC_TREE_RELATION)));
+    }
 
+    @Given("a view with the following properties")
+    public void aViewWithTheFollowingProperties(ViewSpecification viewSpecification) {
+        applicationEventPublisher.publishEvent(new ViewAddedEvent(viewSpecification));
+    }
+
+    @And("the following Fragments are available")
+    public void theFollowingFragmentsAreAvailable(List<Fragment>fragments) {
+        fragments.forEach(fragment -> {
+            when(fragmentRepository.retrieveFragment(fragment.getFragmentId())).thenReturn(Optional.of(fragment));
+        });
+    }
+
+    @Then("wait sometime")
+    public void waitSometime() throws InterruptedException {
+        Thread.sleep(1000);
+    }
 }
--- ldes-server-compaction/src/test/resources/features/compaction.feature
+++ ldes-server-compaction/src/test/resources/features/compaction.feature
@@ -4,4 +4,15 @@ Feature: Execute CompactionService
     Given a view with the following properties
       | viewName                    | pageSize |
       | mobility-hindrances/by-page | 10       |
-   Scenario: Execution Compaction
+    And the following Fragments are available
+      | fragmentIdentifier                       | immutable | relation                                 |
+      | mobility-hindrances/by-page              | false     | mobility-hindrances/by-page?pageNumber=1 |
+      | mobility-hindrances/by-page?pageNumber=1 | true      | mobility-hindrances/by-page?pageNumber=2 |
+      | mobility-hindrances/by-page?pageNumber=2 | true      | mobility-hindrances/by-page?pageNumber=3 |
+      | mobility-hindrances/by-page?pageNumber=3 | true      | mobility-hindrances/by-page?pageNumber=4 |
+      | mobility-hindrances/by-page?pageNumber=4 | true      | mobility-hindrances/by-page?pageNumber=5 |
+      | mobility-hindrances/by-page?pageNumber=5 | true      | mobility-hindrances/by-page?pageNumber=6 |
+      | mobility-hindrances/by-page?pageNumber=6 | false     | [blank]                                  |
+
+  Scenario: Execution Compaction
+    Then wait sometime
