d7f4d119f162450bfb1678ad63623896e94a4feb
Wouter Lefever <wouter.lefever@cegeka.com>
2023-08-25 11:30:47 +0200

    tests

--- ldes-server-compaction/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/compaction/application/services/CompactionScheduler.java
+++ ldes-server-compaction/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/compaction/application/services/CompactionScheduler.java
@@ -12,41 +12,43 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CompactionScheduler {
-    private final ViewCollection viewCollection;
-    private final FragmentRepository fragmentRepository;
-    private final PaginationCompactionService paginationCompactionService;
-    private final RootFragmentRetriever rootFragmentRetriever;
-    private final ObservationRegistry observationRegistry;
+	private final ViewCollection viewCollection;
+	private final FragmentRepository fragmentRepository;
+	private final PaginationCompactionService paginationCompactionService;
+	private final RootFragmentRetriever rootFragmentRetriever;
+	private final ObservationRegistry observationRegistry;
 
-    public CompactionScheduler(ViewCollection viewCollection, FragmentRepository fragmentRepository,
-                               PaginationCompactionService paginationCompactionService, ObservationRegistry observationRegistry) {
-        this.viewCollection = viewCollection;
-        this.fragmentRepository = fragmentRepository;
-        this.paginationCompactionService = paginationCompactionService;
-        this.rootFragmentRetriever = new RootFragmentRetriever(fragmentRepository, observationRegistry);
-        this.observationRegistry = observationRegistry;
-    }
+	public CompactionScheduler(ViewCollection viewCollection, FragmentRepository fragmentRepository,
+			PaginationCompactionService paginationCompactionService, ObservationRegistry observationRegistry) {
+		this.viewCollection = viewCollection;
+		this.fragmentRepository = fragmentRepository;
+		this.paginationCompactionService = paginationCompactionService;
+		this.rootFragmentRetriever = new RootFragmentRetriever(fragmentRepository, observationRegistry);
+		this.observationRegistry = observationRegistry;
+	}
 
-    @Scheduled(fixedDelay = 10000)
-    public void compactFragments() {
-        viewCollection.getAllViewCapacities()
-                .parallelStream()
-                .forEach(viewCapacity -> {
-                    Fragment rootFragment = getRootFragment(viewCapacity);
-                    PaginationStartingNodeIterator paginationStartingNodeIterator = new PaginationStartingNodeIteratorImpl(
-                            fragmentRepository, rootFragment);
-                    while (paginationStartingNodeIterator.hasNext()) {
-                        paginationCompactionService
-                                .applyCompactionStartingFromNode(paginationStartingNodeIterator.next());
-                    }
+	@Scheduled(fixedDelay = 10000)
+	public void compactFragments() {
+		viewCollection.getAllViewCapacities()
+				.parallelStream()
+				.forEach(viewCapacity -> {
+					Fragment rootFragment = getRootFragment(viewCapacity);
+					PaginationStartingNodeIterator paginationStartingNodeIterator = new PaginationStartingNodeIteratorImpl(
+							fragmentRepository, rootFragment);
+					while (paginationStartingNodeIterator.hasNext()) {
+						paginationCompactionService
+								.applyCompactionStartingFromNode(paginationStartingNodeIterator.next());
+					}
 
-                });
-    }
+				});
+	}
 
-    private Fragment getRootFragment(ViewCapacity viewCapacity) {
-        Observation compactionObservation = Observation.createNotStarted("compaction-retrieve-root", observationRegistry).start();
-        Fragment rootFragment = rootFragmentRetriever.retrieveRootFragmentOfView(viewCapacity.getViewName(), compactionObservation);
-        compactionObservation.stop();
-        return rootFragment;
-    }
+	private Fragment getRootFragment(ViewCapacity viewCapacity) {
+		Observation compactionObservation = Observation
+				.createNotStarted("compaction-retrieve-root", observationRegistry).start();
+		Fragment rootFragment = rootFragmentRetriever.retrieveRootFragmentOfView(viewCapacity.getViewName(),
+				compactionObservation);
+		compactionObservation.stop();
+		return rootFragment;
+	}
 }
