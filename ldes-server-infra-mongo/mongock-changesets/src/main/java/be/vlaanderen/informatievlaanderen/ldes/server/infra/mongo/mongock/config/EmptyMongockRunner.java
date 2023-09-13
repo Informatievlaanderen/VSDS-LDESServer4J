fdd693302b8f7b87409080be53b56ccb57568b01
Tom Van Besien <tom.vanbesien@gmail.com>
2023-09-13 14:16:50 +0200

    Remove loggings

---
+++ ldes-server-infra-mongo/mongock-changesets/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/mongock/config/EmptyMongockRunner.java
@@ -0,0 +1,30 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.config;
+
+import io.mongock.api.exception.MongockException;
+import io.mongock.runner.core.executor.MongockRunner;
+
+/**
+ * Mongock runner that does nothing. Used when no migration packages are
+ * provided.
+ */
+public class EmptyMongockRunner implements MongockRunner {
+    @Override
+    public boolean isExecutionInProgress() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public void forceEnable() {
+
+    }
+
+    @Override
+    public void execute() throws MongockException {
+
+    }
+}
