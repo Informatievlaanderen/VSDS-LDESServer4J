baa3c4232f2609b041c899b7d312fb6b4022b321
jonasbul <jonas.bulcke@cegeka.com>
2023-09-22 16:25:56 +0200

    feat: format

--- ldes-server-infra-mongo/mongock-changesets/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/mongock/changeset10/entities/EventStreamEntityV1.java
+++ ldes-server-infra-mongo/mongock-changesets/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/mongock/changeset10/entities/EventStreamEntityV1.java
@@ -8,43 +8,42 @@ import static be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock
 @Document(collection = COLLECTION_NAME)
 public class EventStreamEntityV1 {
 
-    public static final String COLLECTION_NAME = "eventstreams";
+	public static final String COLLECTION_NAME = "eventstreams";
 
-    @Id
-    private final String id;
-    private final String timestampPath;
-    private final String versionOfPath;
-    private final String memberType;
+	@Id
+	private final String id;
+	private final String timestampPath;
+	private final String versionOfPath;
+	private final String memberType;
 
-    public EventStreamEntityV1(String collectionName, String timestampPath, String versionOfPath, String memberType) {
-        this.id = collectionName;
-        this.timestampPath = timestampPath;
-        this.versionOfPath = versionOfPath;
-        this.memberType = memberType;
-    }
+	public EventStreamEntityV1(String collectionName, String timestampPath, String versionOfPath, String memberType) {
+		this.id = collectionName;
+		this.timestampPath = timestampPath;
+		this.versionOfPath = versionOfPath;
+		this.memberType = memberType;
+	}
 
-    public static EventStreamEntityV1 from(EventStreamEntityV2 eventStreamEntityV2) {
-        return new EventStreamEntityV1(
-                eventStreamEntityV2.getId(),
-                eventStreamEntityV2.getTimestampPath(),
-                eventStreamEntityV2.getVersionOfPath(),
-                eventStreamEntityV2.getMemberType()
-        );
-    }
+	public static EventStreamEntityV1 from(EventStreamEntityV2 eventStreamEntityV2) {
+		return new EventStreamEntityV1(
+				eventStreamEntityV2.getId(),
+				eventStreamEntityV2.getTimestampPath(),
+				eventStreamEntityV2.getVersionOfPath(),
+				eventStreamEntityV2.getMemberType());
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getTimestampPath() {
-        return timestampPath;
-    }
+	public String getTimestampPath() {
+		return timestampPath;
+	}
 
-    public String getVersionOfPath() {
-        return versionOfPath;
-    }
+	public String getVersionOfPath() {
+		return versionOfPath;
+	}
 
-    public String getMemberType() {
-        return memberType;
-    }
+	public String getMemberType() {
+		return memberType;
+	}
 }
