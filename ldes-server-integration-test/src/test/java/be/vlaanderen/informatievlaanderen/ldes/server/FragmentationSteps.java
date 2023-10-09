7834aed6311a6694d25e6c029ef8b99ea1e8948c
Jan Robert <15772440+Yalz@users.noreply.github.com>
2023-10-09 10:34:15 +0200

    test: migrated e2e: (#932) fragmentation tests
    * test: migrated e2e:
    - fragmentation tests
    - removed timebased fragmentation
    
    * test: migrated e2e:
    - fragmentation tests
    - removed timebased fragmentation
    
    * test: migrated e2e:
    - add timebased fragmentation tests
    
    * test: migrated e2e:
    - add timebased fragmentation tests
    
    * test: migrated e2e:
    - add timebased fragmentation tests
    
    * test: migrated e2e:
    - add timebased fragmentation tests
    
    * test: migrated e2e:
    - add timebased fragmentation tests

---
+++ ldes-server-integration-test/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/FragmentationSteps.java
@@ -0,0 +1,136 @@
+package be.vlaanderen.informatievlaanderen.ldes.server;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFParser;
+import org.apache.jena.vocabulary.RDF;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
+import static org.apache.jena.rdf.model.ResourceFactory.createResource;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class FragmentationSteps extends LdesServerIntegrationTest {
+	private static final String TREE = "https://w3id.org/tree#";
+	private static final Property TREE_MEMBER = createProperty(TREE, "member");
+	private Model currentFragment;
+	private String currentPath;
+	private String currentFragmentCacheControl;
+
+	@When("I fetch the root {string} fragment of {string}")
+	public void iFetchTheRootFragment(String view, String collection) throws Exception {
+		currentPath = "/%s/%s".formatted(collection, view);
+		MockHttpServletResponse response = mockMvc.perform(get(currentPath).accept("text/turtle"))
+				.andReturn()
+				.getResponse();
+		currentFragmentCacheControl = response.getHeader("Cache-Control");
+		currentFragment = RDFParser.fromString(response.getContentAsString()).lang(Lang.TURTLE).toModel();
+	}
+
+	private void fetchFragment(String path) throws Exception {
+		currentPath = path;
+		MockHttpServletResponse response = mockMvc.perform(get(path)
+				.accept("text/turtle"))
+				.andReturn()
+				.getResponse();
+		currentFragmentCacheControl = response.getHeader("Cache-Control");
+		currentFragment = RDFParser.fromString(response.getContentAsString()).lang(Lang.TURTLE).toModel();
+	}
+
+	@And("I fetch the next fragment through the first {string}")
+	public void iFetchTheNextFragmentThroughTheFirst(String relation) throws Exception {
+		Resource relationSubj = currentFragment.listStatements(null, RDF.type, createResource(TREE + relation))
+				.next().getSubject();
+
+		currentPath = currentFragment.listStatements(relationSubj, createProperty(TREE, "node"), (Resource) null)
+				.next().getObject().toString();
+
+		MockHttpServletResponse response = mockMvc.perform(get(currentPath).accept("text/turtle"))
+				.andReturn()
+				.getResponse();
+		currentFragmentCacheControl = response.getHeader("Cache-Control");
+		currentFragment = RDFParser.fromString(response.getContentAsString()).lang(Lang.TURTLE).toModel();
+	}
+
+	@Then("this fragment only has {int} {string} relation")
+	public void thisFragmentOnlyHasOne(int expectedRelationCount, String relation) {
+		await().atMost(Duration.of(20, ChronoUnit.SECONDS)).until(() -> {
+			fetchFragment(currentPath);
+			int relationCount = currentFragment.listStatements(null, RDF.type, createResource(TREE + relation))
+					.toList().size();
+			return relationCount == expectedRelationCount;
+		});
+	}
+
+	@And("this fragment is immutable")
+	public void thisFragmentIsImmutable() {
+		assertTrue(currentFragmentCacheControl.contains("immutable"));
+	}
+
+	@And("this fragment contains {int} members")
+	public void thisFragmentContainsMembers(int expectedMemberCount) {
+		await().atMost(Duration.of(20, ChronoUnit.SECONDS)).until(() -> {
+			fetchFragment(currentPath);
+			return currentFragment.listObjectsOfProperty(TREE_MEMBER).toList().size() == expectedMemberCount;
+		});
+	}
+
+	@And("this fragment is mutable")
+	public void thisFragmentIsNotImmutable() {
+		assertFalse(currentFragmentCacheControl.contains("immutable"));
+	}
+
+	@And("this fragment has no relations")
+	public void thisFragmentHasNoRelations() {
+		await().atMost(Duration.of(20, ChronoUnit.SECONDS)).until(() -> {
+			fetchFragment(currentPath);
+			return !currentFragment.listObjectsOfProperty(createProperty(TREE + "relation")).hasNext();
+		});
+	}
+
+	@When("I fetch the geo-spatial fragment for tile {string} from the {string} view of {string}")
+	public void iFetchTheGeoSpatialFragmentForTileFromTheViewOf(String tile, String view, String collection)
+			throws Exception {
+		currentPath = "/%s/%s?tile=%s".formatted(collection, view, tile);
+		MockHttpServletResponse response = mockMvc.perform(get(currentPath).accept("text/turtle"))
+				.andReturn()
+				.getResponse();
+		currentFragmentCacheControl = response.getHeader("Cache-Control");
+		currentFragment = RDFParser.fromString(response.getContentAsString()).lang(Lang.TURTLE).toModel();
+	}
+
+	@When("I fetch the timebased fragment {string} fragment of this month of {string}")
+	public void iFetchTheTimebasedFragmentFragmentOfTodayOf(String view, String collection) throws Exception {
+		LocalDateTime now = LocalDateTime.now();
+		currentPath = "/%s/%s?year=%s&month=%s".formatted(collection, view, now.getYear(), now.getMonthValue());
+		String response = mockMvc.perform(get(currentPath).accept("text/turtle"))
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+
+		// Edge case for test being run at end of a day
+		if (response.contains("No fragment exists")) {
+			now = now.minusDays(1);
+			currentPath = "/%s/%s?year=%s&month=%s&day=%s"
+					.formatted(collection, view, now.getYear(), now.getMonthValue(), now.getDayOfMonth());
+			response = mockMvc.perform(get(currentPath).accept("text/turtle"))
+					.andReturn()
+					.getResponse()
+					.getContentAsString();
+		}
+		currentFragment = RDFParser.fromString(response).lang(Lang.TURTLE).toModel();
+	}
+
+}
--- ldes-server-integration-test/src/test/resources/features/retention.feature
+++ ldes-server-integration-test/src/test/resources/features/retention.feature
@@ -2,33 +2,29 @@ Feature: LDES Server Retention
 
   @time-based
   Scenario: Server provides timebased retention
-    Given I create the eventstream "data/input/eventstreams/mobility-hindrances_timebased.ttl"
+    Given I create the eventstream "data/input/eventstreams/retention/mobility-hindrances_timebased.ttl"
     When I ingest 30 members to the collection "mobility-hindrances"
     Then the first fragment of the "paged" view in collection "mobility-hindrances" contains 30 members
     # Since all added members' timestamp values equal to their ingestion date, they should be removed after 15 seconds
     Then the first fragment of the "paged" view in collection "mobility-hindrances" contains 0 members
-    And I delete the eventstream "mobility-hindrances"
 
   @version-based
   Scenario: Server provides version retention
-    Given I create the eventstream "data/input/eventstreams/mobility-hindrances_versionbased.ttl"
+    Given I create the eventstream "data/input/eventstreams/retention/mobility-hindrances_versionbased.ttl"
     When I ingest 30 members to the collection "mobility-hindrances"
     # Since all added members belong to the same version, only 10 are kept as defined by the retention policy
     Then the first fragment of the "paged" view in collection "mobility-hindrances" contains 10 members
-    And I delete the eventstream "mobility-hindrances"
 
   @point-in-time
   Scenario: Server provides point in time retention
-    Given I create the eventstream "data/input/eventstreams/mobility-hindrances_pointintime.ttl"
+    Given I create the eventstream "data/input/eventstreams/retention/mobility-hindrances_pointintime.ttl"
     When I ingest 30 members to the collection "mobility-hindrances"
     # Since all added members have a timestamp after the set point in time, all members are retained
     Then the first fragment of the "paged" view in collection "mobility-hindrances" contains 30 members
-    And I delete the eventstream "mobility-hindrances"
 
   @combined @version-based-and-point-in-time
   Scenario: Server combines multiple retention policies
-    Given I create the eventstream "data/input/eventstreams/mobility-hindrances_combined.ttl"
+    Given I create the eventstream "data/input/eventstreams/retention/mobility-hindrances_combined.ttl"
     When I ingest 30 members to the collection "mobility-hindrances"
     # With the version based and timebased retention combined, only 5 members will remain even if they are more than 5s ago
     Then the first fragment of the "paged" view in collection "mobility-hindrances" contains 5 members
-    And I delete the eventstream "mobility-hindrances"
