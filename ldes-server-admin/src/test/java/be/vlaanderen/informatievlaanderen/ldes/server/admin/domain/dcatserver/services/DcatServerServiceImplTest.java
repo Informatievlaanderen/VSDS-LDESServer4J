e28ce26758526927c23004a211a9a149322e8f6c
jonasbul <jonas.bulcke@cegeka.com>
2023-11-03 11:39:46 +0100

    fix: rebase issue

--- ldes-server-admin/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/domain/dcatserver/services/DcatServerServiceImplTest.java
+++ ldes-server-admin/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/domain/dcatserver/services/DcatServerServiceImplTest.java
@@ -29,8 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
