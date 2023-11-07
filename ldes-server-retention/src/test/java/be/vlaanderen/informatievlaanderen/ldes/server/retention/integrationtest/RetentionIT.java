ee1e997c59af1d009cb6129f64fe3948db194baa
Tom Van Besien <tom.vanbesien@gmail.com>
2023-11-07 18:25:31 +0100

    feat: disable scheduled compaction and deletion

--- ldes-server-retention/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/retention/integrationtest/RetentionIT.java
+++ ldes-server-retention/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/retention/integrationtest/RetentionIT.java
@@ -4,8 +4,8 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+//@Suite
+//@IncludeEngines("cucumber")
+//@SelectClasspathResource("features")
 public class RetentionIT extends RetentionIntegrationTest {
 }
