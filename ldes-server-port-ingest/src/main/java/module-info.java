e787a41b48c890b3d9aafbb70732929d6b63c3aa
larsvke <lars.vankerckhoven@cegeka.com>
2023-11-22 16:08:12 +0100

    fix : Optimise Ldes Member Ingestion
    #998
    
    avoid making two calls to db
    
    ch.qos dependencies added

--- ldes-server-port-ingest/src/main/java/module-info.java
+++ ldes-server-port-ingest/src/main/java/module-info.java
@@ -4,6 +4,8 @@ module ldes.ingest.domain {
     requires org.apache.jena.core;
     requires org.apache.jena.shacl;
     requires org.slf4j;
+    requires ch.qos.logback.classic;
+    requires ch.qos.logback.core;
     requires spring.boot.autoconfigure;
     requires org.apache.jena.arq;
     requires micrometer.core;
