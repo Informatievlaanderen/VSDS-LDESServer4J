a2f5ded61af6f48ed9f9a4b5a3908beaa52269c3
larsvke <lars.vankerckhoven@cegeka.com>
2023-12-04 12:53:24 +0100

    fix : Metrics For Prometheus

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/constants/CounterConstants.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/constants/CounterConstants.java
@@ -2,7 +2,5 @@ package be.vlaanderen.informatievlaanderen.ldes.server.domain.constants;
 
 public class CounterConstants {
 
-    private CounterConstants(){};
-
     public static final String LDES_SERVER_CREATE_FRAGMENTS_COUNT = "ldes_server_create_fragments_count";
 }
