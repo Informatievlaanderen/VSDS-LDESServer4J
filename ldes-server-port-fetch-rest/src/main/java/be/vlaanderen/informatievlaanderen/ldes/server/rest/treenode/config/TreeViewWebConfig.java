c16f5d6cda4eec837d09abc2f8b82bd5e8646a08
jonasbul <jonas.bulcke@cegeka.com>
2023-12-19 12:54:36 +0100

    feat: binary representation and parsing of RDF models

--- ldes-server-port-fetch-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/rest/treenode/config/TreeViewWebConfig.java
+++ ldes-server-port-fetch-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/rest/treenode/config/TreeViewWebConfig.java
@@ -11,6 +11,8 @@ import org.springframework.http.converter.HttpMessageConverter;
 @Configuration
 public class TreeViewWebConfig {
 
+	public static final String DEFAULT_RDF_MEDIA_TYPE = "text/turtle";
+
 	@Bean
 	public HttpMessageConverter<TreeNode> treeNodeHttpConverter(
 			final TreeNodeConverter treeNodeConverter, final RdfModelConverter rdfModelConverter) {
