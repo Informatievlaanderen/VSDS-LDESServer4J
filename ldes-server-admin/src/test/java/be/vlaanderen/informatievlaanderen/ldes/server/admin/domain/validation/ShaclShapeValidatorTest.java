d5030041f8c94f9db45d25d3965d5b16c15a72ca
jonasbul <jonas.bulcke@cegeka.com>
2024-01-16 11:16:24 +0100

    fix: wrong charset import

--- ldes-server-admin/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/domain/validation/ShaclShapeValidatorTest.java
+++ ldes-server-admin/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/domain/validation/ShaclShapeValidatorTest.java
@@ -2,7 +2,6 @@ package be.vlaanderen.informatievlaanderen.ldes.server.admin.domain.validation;
 
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.exceptions.ShaclValidationException;
 import org.apache.commons.io.FileUtils;
-import org.apache.jena.ext.com.google.common.base.Charsets;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
@@ -13,9 +12,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -44,7 +43,7 @@ class ShaclShapeValidatorTest {
         }
 
         @Test
-        void when_validateInvalidShaclShape_thenReturnInvalid() throws URISyntaxException {
+        void when_validateInvalidShaclShape_thenReturnInvalid() {
             final Model model = RDFDataMgr.loadModel("eventstream/streams/invalid-shape.ttl");
 
             assertThatThrownBy(() -> validator.validate(model)).isInstanceOf(ShaclValidationException.class);
@@ -65,7 +64,7 @@ class ShaclShapeValidatorTest {
         @Test
         void given_InvalidViewWithHierarchicalFragmentation_when_validateView_then_ThrowNoException() throws IOException {
             final File file = ResourceUtils.getFile("classpath:view-with-hierarchical-timebased-frag.ttl");
-            final String modelString = FileUtils.readFileToString(file, Charsets.UTF_8).replace("day", "invalid-value");
+            final String modelString = FileUtils.readFileToString(file, StandardCharsets.UTF_8).replace("day", "invalid-value");
             final Model model = RDFParser.fromString(modelString).lang(Lang.TURTLE).toModel();
 
             assertThatThrownBy(() -> validator.validate(model))
