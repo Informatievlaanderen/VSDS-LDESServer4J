4189b1978f8047b41a3290f4c3d3b9885afa8f6c
Jonas Bulcke <jonas.bulcke@cegeka.com>
2024-03-14 14:50:47 +0100

    fix: missing mongock annotation

--- ldes-server-infra-mongo/mongock-changesets/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/mongock/changeset15/MemberUpdaterChange.java
+++ ldes-server-infra-mongo/mongock-changesets/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/infra/mongo/mongock/changeset15/MemberUpdaterChange.java
@@ -1,23 +1,21 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset15;
 
-import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset12.entities.EventStreamEntityV1;
-import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset12.entities.EventStreamEntityV2;
 import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset15.entities.EventStreamEntity;
 import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset15.entities.MemberEntityV1;
-import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset15.entities.MemberEntityV2;
 import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset15.services.MemberV2Builder;
 import be.vlaanderen.informatievlaanderen.ldes.server.infra.mongo.mongock.changeset15.valueobjects.EventStreamProperties;
+import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
 import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@ChangeUnit(id = "member-updater-changeset-15", order = "2024-03-14 00:00:00", author = "VSDS")
 public class MemberUpdaterChange {
     public static final String MEMBER_COLLECTION_NAME = "ingest_ldesmember";
 
