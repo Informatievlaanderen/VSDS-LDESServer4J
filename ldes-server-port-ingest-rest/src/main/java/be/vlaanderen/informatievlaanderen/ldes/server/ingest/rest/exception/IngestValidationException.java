6d32ab091e9062008e1f6f772a80000592a66db9
pj-cegeka <119848850+pj-cegeka@users.noreply.github.com>
2024-04-02 09:22:55 +0200

    feat: return validation message in shacl report format (#1227)

--- ldes-server-port-ingest-rest/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingest/rest/exception/IngestValidationException.java
+++
@@ -1,8 +0,0 @@
-package be.vlaanderen.informatievlaanderen.ldes.server.ingest.rest.exception;
-
-public class IngestValidationException extends RuntimeException {
-
-    public IngestValidationException(String message) {
-        super(message);
-    }
-}
