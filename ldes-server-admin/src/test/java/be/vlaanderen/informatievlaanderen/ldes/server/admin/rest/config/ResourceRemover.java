da6a73b6e1a407802b646d89bbabbef8868d23d8
pj-cegeka <119848850+pj-cegeka@users.noreply.github.com>
2024-05-06 14:19:20 +0200

    fix: remove resources between tests (#1255)
    * fix: remove eventstreams between tests
    
    * fix: throw event when eventstream is created

---
+++ ldes-server-admin/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/config/ResourceRemover.java
@@ -0,0 +1,35 @@
+package be.vlaanderen.informatievlaanderen.ldes.server.admin.rest.config;
+
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.events.admin.EventStreamCreatedEvent;
+import be.vlaanderen.informatievlaanderen.ldes.server.domain.events.admin.EventStreamDeletedEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class ResourceRemover {
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private List<String> usedStreams = new ArrayList<>();
+
+    private ResourceRemover(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @EventListener
+    public void handleEventStreamCreatedEvent(EventStreamCreatedEvent event) {
+        usedStreams.add(event.eventStream().getCollection());
+    }
+
+    @EventListener
+    public void handleEventStreamDeletedEvent(EventStreamDeletedEvent event) {
+        usedStreams.remove(event.collectionName());
+    }
+
+    public void removeUsedResources() {
+        List.copyOf(usedStreams).forEach(name -> applicationEventPublisher.publishEvent(new EventStreamDeletedEvent(name)));
+    }
+}
