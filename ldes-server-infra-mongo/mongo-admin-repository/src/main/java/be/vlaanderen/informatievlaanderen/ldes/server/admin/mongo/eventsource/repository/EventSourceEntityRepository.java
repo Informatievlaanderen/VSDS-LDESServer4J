6ed77ee4e00b4e8a891cc72e42301c7656057dd4
Yalz <Yalz@users.noreply.github.com>
2024-05-17 15:24:32 +0200

    checkpoint: docs

--- ldes-server-infra-mongo/mongo-admin-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/mongo/eventsource/repository/EventSourceEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package be.vlaanderen.informatievlaanderen.ldes.server.admin.mongo.eventsource.repository;
-
-import be.vlaanderen.informatievlaanderen.ldes.server.admin.mongo.eventsource.entity.EventSourceEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EventSourceEntityRepository extends MongoRepository<EventSourceEntity, String> {
-}
