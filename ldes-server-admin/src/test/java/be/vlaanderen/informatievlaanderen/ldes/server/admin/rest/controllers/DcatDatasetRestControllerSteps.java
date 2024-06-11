f4dc3dffe9245ee3520f27e8d789491fa19d6094
Jonas Bulcke <jonas.bulcke@cegeka.com>
2024-06-11 14:09:20 +0200

    fix: broken test

--- ldes-server-admin/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/controllers/DcatDatasetRestControllerSteps.java
+++ ldes-server-admin/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/admin/rest/controllers/DcatDatasetRestControllerSteps.java
@@ -9,7 +9,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.commons.io.FileUtils;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFParser;
 import org.springframework.test.web.servlet.ResultActions;
@@ -18,7 +17,6 @@ import org.springframework.util.ResourceUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 
 import static be.vlaanderen.informatievlaanderen.ldes.server.admin.rest.controllers.DcatDatasetRestController.BASE_URL;
 import static org.mockito.Mockito.*;
@@ -26,87 +24,86 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class DcatDatasetRestControllerSteps extends SpringIntegrationTest {
-	private final static String COLLECTION_NAME = "collectionName";
-	private ResultActions resultActions;
-	private String datasetString;
-	@After
-	public void cleanup() {
-		resourceRemover.removeUsedResources();
-	}
+    private final static String COLLECTION_NAME = "collectionName";
+    private ResultActions resultActions;
+    private String datasetString;
 
-	@Given("I have a valid dcat dataset")
-	public void iHaveAValidDcatDataset() throws Exception {
-		datasetString = readDataFromFile("dcat/dataset/valid.ttl");
-	}
+    @After
+    public void cleanup() {
+        resourceRemover.removeUsedResources();
+    }
 
-	@Given("I have a invalid dcat dataset")
-	public void iHaveAnInvalidDcatDataset() throws Exception {
-		datasetString = readDataFromFile("dcat/dataset/not-valid.ttl");
-	}
+    @Given("I have a valid dcat dataset")
+    public void iHaveAValidDcatDataset() throws Exception {
+        datasetString = readDataFromFile("dcat/dataset/valid.ttl");
+    }
 
-	@When("I POST this dataset")
-	public void iCanPOSTThisDataset() throws Exception {
-		resultActions = mockMvc.perform(post(BASE_URL, COLLECTION_NAME)
-				.contentType(Lang.TURTLE.getHeaderString())
-				.content(datasetString));
-	}
+    @Given("I have a invalid dcat dataset")
+    public void iHaveAnInvalidDcatDataset() throws Exception {
+        datasetString = readDataFromFile("dcat/dataset/not-valid.ttl");
+    }
 
-	@When("I PUT this dataset")
-	public void iPUTThisDataset() throws Exception {
-		resultActions = mockMvc.perform(put(BASE_URL, COLLECTION_NAME)
-				.contentType(Lang.TURTLE.getHeaderString())
-				.content(datasetString));
-	}
+    @When("I POST this dataset")
+    public void iCanPOSTThisDataset() throws Exception {
+        resultActions = mockMvc.perform(post(BASE_URL, COLLECTION_NAME)
+                .contentType(Lang.TURTLE.getHeaderString())
+                .content(datasetString));
+    }
 
-	@When("I DELETE this dataset")
-	public void iDELETEThisDataservice() throws Exception {
-		resultActions = mockMvc.perform(delete(BASE_URL, COLLECTION_NAME));
-	}
+    @When("I PUT this dataset")
+    public void iPUTThisDataset() throws Exception {
+        resultActions = mockMvc.perform(put(BASE_URL, COLLECTION_NAME)
+                .contentType(Lang.TURTLE.getHeaderString())
+                .content(datasetString));
+    }
 
-	private String readDataFromFile(String filename) throws IOException {
-		File file = ResourceUtils.getFile("classpath:" + filename);
-		return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
-	}
+    @When("I DELETE this dataset")
+    public void iDELETEThisDataservice() throws Exception {
+        resultActions = mockMvc.perform(delete(BASE_URL, COLLECTION_NAME));
+    }
 
-	@And("The dataset will be stored")
-	public void theMetadataWillBeStored() {
-		Model model = RDFParser.fromString(datasetString).lang(Lang.TURTLE).build().toModel();
-		DcatDataset dataset = new DcatDataset(COLLECTION_NAME, model);
-		verify(dcatDatasetRepository, times(1)).saveDataset(dataset);
-	}
+    private String readDataFromFile(String filename) throws IOException {
+        File file = ResourceUtils.getFile("classpath:" + filename);
+        return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+    }
 
-	@And("The dataset will not be stored")
-	public void theMetadataWillNotBeStored() {
-		Model model = RDFParser.fromString(datasetString).lang(Lang.TURTLE).build().toModel();
-		DcatDataset dataset = new DcatDataset(COLLECTION_NAME, model);
-		verify(dcatDatasetRepository, never()).saveDataset(dataset);
-	}
+    @And("The dataset will be stored")
+    public void theMetadataWillBeStored() {
+        Model model = RDFParser.fromString(datasetString).lang(Lang.TURTLE).build().toModel();
+        DcatDataset dataset = new DcatDataset(COLLECTION_NAME, model);
+        verify(dcatDatasetRepository, times(1)).saveDataset(dataset);
+    }
 
-	@Then("The dataset will be deleted")
-	public void theDataserviceMetadataWillBeDeleted() {
-		verify(dcatDatasetRepository, times(1)).deleteDataset(COLLECTION_NAME);
-	}
+    @And("The dataset will not be stored")
+    public void theMetadataWillNotBeStored() {
+        Model model = RDFParser.fromString(datasetString).lang(Lang.TURTLE).build().toModel();
+        DcatDataset dataset = new DcatDataset(COLLECTION_NAME, model);
+        verify(dcatDatasetRepository, never()).saveDataset(dataset);
+    }
 
-	@Then("The dataset will not be deleted")
-	public void theDataserviceMetadataWillNotBeDeleted() {
-		verify(dcatDatasetRepository, never()).deleteDataset(COLLECTION_NAME);
-	}
+    @Then("The dataset will be deleted")
+    public void theDataserviceMetadataWillBeDeleted() {
+        verify(dcatDatasetRepository, times(1)).deleteDataset(COLLECTION_NAME);
+    }
 
-	@And("Response with http {int} will be returned for dataset")
-	public void responseWithHttpWillBeReturned(int httpCode) throws Exception {
-		resultActions.andExpect(status().is(httpCode));
-	}
+    @Then("The dataset will not be deleted")
+    public void theDataserviceMetadataWillNotBeDeleted() {
+        verify(dcatDatasetRepository, never()).deleteDataset(COLLECTION_NAME);
+    }
 
-	@And("The dataset already exists")
-	public void theDataserviceAlreadyExists() {
-		Model defaultModel = ModelFactory.createDefaultModel();
-		when(dcatDatasetRepository.retrieveDataset(COLLECTION_NAME))
-				.thenReturn(Optional.of(new DcatDataset(COLLECTION_NAME, defaultModel)));
-	}
+    @And("Response with http {int} will be returned for dataset")
+    public void responseWithHttpWillBeReturned(int httpCode) throws Exception {
+        resultActions.andExpect(status().is(httpCode));
+    }
 
-	@And("The dataset does not yet exist")
-	public void theDataserviceDoesNotYetExist() {
-		when(dcatDatasetRepository.retrieveDataset(COLLECTION_NAME)).thenReturn(Optional.empty());
-	}
+    @And("The dataset already exists")
+    public void theDataserviceAlreadyExists() {
+        when(dcatDatasetRepository.exitsByCollectionName(COLLECTION_NAME)).thenReturn(true);
+    }
+
+    @And("The dataset does not yet exist")
+    public void theDataserviceDoesNotYetExist() {
+        when(dcatDatasetRepository.exitsByCollectionName(COLLECTION_NAME)).thenReturn(false);
+    }
 
 }
