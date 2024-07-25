1be9f173a6583a88e5b6807c9201ce4db677a974
Jonas Bulcke <jonas.bulcke@cegeka.com>
2024-07-25 17:31:25 +0200

    fix: broken test

--- ldes-server-pagination/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/pagination/PaginationServiceTest.java
+++ ldes-server-pagination/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/pagination/PaginationServiceTest.java
@@ -7,6 +7,7 @@ import be.vlaanderen.informatievlaanderen.ldes.server.domain.model.ViewSpecifica
 import be.vlaanderen.informatievlaanderen.ldes.server.fetching.entities.MemberAllocation;
 import be.vlaanderen.informatievlaanderen.ldes.server.pagination.batch.PaginationJobDefinitions;
 import be.vlaanderen.informatievlaanderen.ldes.server.pagination.entities.Page;
+import be.vlaanderen.informatievlaanderen.ldes.server.pagination.valueobjects.PageAssignment;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.partition.support.Partitioner;
@@ -44,9 +45,9 @@ class PaginationServiceTest {
 	@MockBean
 	private ItemReader<Page> pageReader;
 	@MockBean
-	private ItemProcessor<Page, List<Page>> pageRelationProcessor;
+	private ItemProcessor<Page, List<PageAssignment>> pageRelationProcessor;
 	@MockBean
-	private ItemWriter<List<Page>> memberAssigner;
+	private ItemWriter<List<PageAssignment>> memberAssigner;
 	@Autowired
 	private PaginationService paginationService;
 	@Autowired
@@ -83,7 +84,7 @@ class PaginationServiceTest {
 
 		paginationService.handleNewViewBucketisedEvent(new NewViewBucketisedEvent(VIEW_NAME_1.asString()));
 
-		verify(memberAssigner).write(argThat(chunk -> chunk.getItems().contains(List.of(page))));
+		verify(memberAssigner).write(argThat(chunk -> chunk.getItems().contains(List.of(new PageAssignment(page.getId(), page.getBucketId(), 22)))));
 	}
 
 	private void mockBucketisationPartitioner() {
@@ -100,6 +101,6 @@ class PaginationServiceTest {
 
 
 	private void stubProcessor() throws Exception {
-		when(pageRelationProcessor.process(page)).thenReturn(List.of(page));
+		when(pageRelationProcessor.process(page)).thenReturn(List.of(new PageAssignment(page.getId(), page.getBucketId(), 22)));
 	}
 }
