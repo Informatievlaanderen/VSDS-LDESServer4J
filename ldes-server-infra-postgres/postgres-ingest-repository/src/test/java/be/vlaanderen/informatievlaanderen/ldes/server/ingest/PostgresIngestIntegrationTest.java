9d5caaa9975370a29a247b9fef7c9919e1eb1479
Yalz <Yalz@users.noreply.github.com>
2024-08-23 16:45:02 +0200

    make delete in test transactional

--- ldes-server-infra-postgres/postgres-ingest-repository/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/ingest/PostgresIngestIntegrationTest.java
+++ ldes-server-infra-postgres/postgres-ingest-repository/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/ingest/PostgresIngestIntegrationTest.java
@@ -1,5 +1,7 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.ingest;
 
+import be.vlaanderen.informatievlaanderen.ldes.server.admin.domain.eventstream.repository.EventStreamRepository;
+import be.vlaanderen.informatievlaanderen.ldes.server.admin.postgres.eventstream.repository.EventStreamEntityRepository;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.services.FragmentationMetricsRepository;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.services.MemberMetricsRepository;
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.services.ServerMetrics;
@@ -15,11 +17,10 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
-
-import javax.sql.DataSource;
 
 import static org.mockito.Mockito.mock;
 
@@ -30,8 +31,10 @@ import static org.mockito.Mockito.mock;
 @ActiveProfiles("postgres-test")
 @EntityScan(basePackages = {"be.vlaanderen.informatievlaanderen.ldes.server"})
 @ComponentScan(basePackages = {"be.vlaanderen.informatievlaanderen.ldes.server.ingest",
-		"be.vlaanderen.informatievlaanderen.ldes.server.domain"})
+		"be.vlaanderen.informatievlaanderen.ldes.server.domain",
+		"be.vlaanderen.informatievlaanderen.ldes.server.admin.postgres.eventstream"})
 @ContextConfiguration(classes = {MemberEntityRepository.class})
+@EnableJpaRepositories(basePackageClasses = {MemberEntityRepository.class, EventStreamEntityRepository.class})
 @Import(PostgresIngestIntegrationTest.IngestTestConfiguration.class)
 @Sql(value = {"init-collections.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(statements = "DELETE FROM collections;", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
@@ -40,9 +43,8 @@ public class PostgresIngestIntegrationTest {
 
 	@Autowired
 	MemberPostgresRepository memberRepository;
-
 	@Autowired
-	DataSource dataSource;
+	EventStreamRepository eventStreamRepository;
 
 	@TestConfiguration
 	static class IngestTestConfiguration {
