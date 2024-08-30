32f78a0397042dea79f1317fcedfb1940e39b31d
Yalz <Yalz@users.noreply.github.com>
2024-08-30 10:50:06 +0200

    chore: update unprocessed views query

--- ldes-server-infra-postgres/postgres-ingest-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingest/postgres/repository/MemberEntityRepository.java
+++ ldes-server-infra-postgres/postgres-ingest-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/ingest/postgres/repository/MemberEntityRepository.java
@@ -18,22 +18,7 @@ public interface MemberEntityRepository extends JpaRepository<MemberEntity, Stri
 			""", nativeQuery = true)
 	int countMemberEntitiesByColl(String collectionName);
 
-	@Query(value = """
-			select c.name, v.name, count(*)
-			from page_members pm
-			         RIGHT JOIN members m on pm.member_id = m.member_id
-			         JOIN collections c on c.collection_id = m.collection_id
-			         JOIN views v on c.collection_id = v.collection_id
-			where (pm.member_id IS NULL OR pm.page_id IS NULL)
-			  AND (NOT EXISTS (SELECT 1 FROM page_members) OR m.member_id >  (select max(member_id)
-			                     from page_members pmx
-			                              JOIN buckets b on b.bucket_id = pmx.bucket_id
-			                              JOIN views v2 on v2.view_id = b.view_id
-			                     WHERE v2.view_id = v.view_id
-			                       AND v2.collection_id = c.collection_id))
-			GROUP BY c.collection_id, v.view_id
-			HAVING (count(*) > 0)
-			""", nativeQuery = true)
+	@Query(value = "select * from unprocessed_views", nativeQuery = true)
 	List<Tuple> getUnprocessedViews();
 
 	@Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN true ELSE false END " +
--- ldes-server-infra-postgres/postgres-liquibase/src/main/resources/db/changelog/3_3_0/add-fragmentation-stats-views.sql
+++ ldes-server-infra-postgres/postgres-liquibase/src/main/resources/db/changelog/3_3_0/add-fragmentation-stats-views.sql
@@ -1,24 +1,38 @@
 CREATE OR REPLACE VIEW member_stats as
-select m.collection_id, count(*) as member_count
+select m.collection_id, v.view_id, coalesce(max(m.member_id),0) as last
 from members m
-group by m.collection_id;
+         inner join views v on v.collection_id = m.collection_id
+group by m.collection_id, v.view_id;
 
 CREATE OR REPLACE VIEW bucket_stats as
-select c.collection_id, v.view_id, count(*) as bucketized
+select c.collection_id, v.view_id, coalesce(
+        (select max(pm.member_id)
+         from page_members pm
+                  inner join buckets b on pm.bucket_id = b.bucket_id
+         where v.view_id = b.view_id)
+    ,0) as last
 from collections c
          inner join views v on c.collection_id = v.collection_id
-         inner join buckets b on v.view_id = b.view_id
-         inner join page_members pm on b.bucket_id = pm.bucket_id
 group by c.collection_id, v.view_id;
 
-CREATE OR REPLACE VIEW page_stats as
-select c.collection_id,
-       v.view_id,
-       (select count(*)
-        from page_members pm
-                 inner join buckets b on pm.bucket_id = b.bucket_id
-        where v.view_id = b.view_id
-          and pm.page_id is null) as unpaged
+CREATE OR REPLACE VIEW needs_bucketization as
+select ms.collection_id, ms.view_id, (ms.last > bs.last) as should_bucketize
+from member_stats ms
+         inner join bucket_stats bs on bs.collection_id = ms.collection_id and bs.view_id = ms.view_id;
+
+CREATE OR REPLACE VIEW needs_pagination as
+select c.collection_id, v.view_id, (exists (
+    select * from page_members pm
+                      inner join buckets b on b.bucket_id = pm.bucket_id
+    where pm.page_id is null)) as should_paginate
 from collections c
          inner join views v on c.collection_id = v.collection_id
-group by c.collection_id, v.view_id
+group by c.collection_id, v.view_id;
+
+CREATE OR REPLACE VIEW unprocessed_views as
+select c.name as collection_name, v.name as view_name
+from collections c
+         inner join views v on c.collection_id = v.collection_id
+         inner join needs_bucketization nb on nb.collection_id = c.collection_id and nb.view_id = v.view_id
+         inner join needs_pagination np on np.collection_id = c.collection_id and np.view_id = v.view_id
+where nb.should_bucketize or np.should_paginate;
