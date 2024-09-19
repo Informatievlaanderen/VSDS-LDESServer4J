46d76428b46e8952e28357e1d2f11d4b7ed726c3
Jonas Bulcke <jonas.bulcke@cegeka.com>
2024-09-19 15:38:04 +0200

    fix: merge conflicts

--- ldes-server-infra-postgres/postgres-pagination-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/pagination/postgres/PageRelationPostgresRepository.java
+++ ldes-server-infra-postgres/postgres-pagination-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/pagination/postgres/PageRelationPostgresRepository.java
@@ -2,7 +2,6 @@ package be.vlaanderen.informatievlaanderen.ldes.server.pagination.postgres;
 
 import be.vlaanderen.informatievlaanderen.ldes.server.domain.constants.RdfConstants;
 import be.vlaanderen.informatievlaanderen.ldes.server.pagination.postgres.repository.PageRelationEntityRepository;
-import be.vlaanderen.informatievlaanderen.ldes.server.pagination.postgres.repository.RelationEntityRepository;
 import be.vlaanderen.informatievlaanderen.ldes.server.pagination.repositories.PageRelationRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,19 +20,6 @@ public class PageRelationPostgresRepository implements PageRelationRepository {
 	@Transactional
 	public void insertGenericBucketRelation(long fromPageId, long toPageId) {
 		pageRelationEntityRepository.insertRelation(fromPageId, toPageId, RdfConstants.GENERIC_TREE_RELATION);
-	}
-
-	@Override
-	@Transactional
-	public void insertBucketRelation(BucketRelation bucketRelation) {
-		pageRelationEntityRepository.insertRelation(
-				bucketRelation.fromBucket().createPartialUrl(),
-				bucketRelation.toBucket().createPartialUrl(),
-				bucketRelation.treeRelationType(),
-				bucketRelation.treeValue(),
-				bucketRelation.treeValueType(),
-				bucketRelation.treePath()
-		);
 	}
 
 	@Override
