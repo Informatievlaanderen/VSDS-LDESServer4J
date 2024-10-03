4438718a44e075fae6915348fc26c3efe3797326
Jonas Bulcke <jonas.bulcke@cegeka.com>
2024-10-03 17:28:18 +0200

    fix: broken tests

--- ldes-server-integration-test/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/resultactionsextensions/ResponseToModelConverter.java
+++ ldes-server-integration-test/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/resultactionsextensions/ResponseToModelConverter.java
@@ -1,5 +1,6 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.resultactionsextensions;
 
+import org.apache.jena.atlas.web.ContentType;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.RDFLanguages;
 import org.apache.jena.riot.RDFParser;
@@ -18,7 +19,7 @@ public class ResponseToModelConverter {
     public Model convert() throws UnsupportedEncodingException {
         return RDFParser
                 .fromString(response.getContentAsString())
-                .lang(RDFLanguages.contentTypeToLang(response.getContentType()))
+                .lang(RDFLanguages.contentTypeToLang(ContentType.create(response.getContentType())))
                 .toModel();
     }
 }
