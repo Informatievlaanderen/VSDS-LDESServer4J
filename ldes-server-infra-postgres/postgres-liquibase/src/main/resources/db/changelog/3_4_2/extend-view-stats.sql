4334d555c98dc4017eb8144b946a55e7d6ffaa87
Ranko Orlic <ranko@aquila-solutions.com>
2024-10-09 14:09:48 +0200

    fix: use coalesce for initializing last ID values

--- ldes-server-infra-postgres/postgres-liquibase/src/main/resources/db/changelog/3_4_2/extend-view-stats.sql
+++ ldes-server-infra-postgres/postgres-liquibase/src/main/resources/db/changelog/3_4_2/extend-view-stats.sql
@@ -4,8 +4,12 @@ ALTER TABLE "view_stats" ADD COLUMN "paginated_last_id" bigint NOT NULL default 
 ALTER TABLE "view_stats" ADD PRIMARY KEY ("view_id");
 
 -- initialize last ID values
-UPDATE view_stats vs SET bucketized_last_id = (select max(distinct(member_id)) from page_members pm WHERE pm.view_id = vs.view_id);
-UPDATE view_stats vs SET paginated_last_id = (select max(distinct(member_id)) from page_members pm WHERE pm.view_id = vs.view_id and pm.page_id is not null);
+UPDATE view_stats vs SET bucketized_last_id =coalesce((
+  select max(distinct(member_id)) from page_members pm WHERE pm.view_id = vs.view_id
+), (0)::bigint);
+UPDATE view_stats vs SET paginated_last_id = coalesce((
+  select max(distinct(member_id)) from page_members pm WHERE pm.view_id = vs.view_id and pm.page_id is not null
+), (0)::bigint);
 
 -- fix function to also set view stats last bucketized ID
 create or replace function on_page_member_inserted() returns trigger language plpgsql as $$
