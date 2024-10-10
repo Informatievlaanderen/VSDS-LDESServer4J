85114f81e548c4acf59199a04fd3c9e5232e76c9
Ranko Orlic <ranko@aquila-solutions.com>
2024-10-10 11:01:24 +0200

    chore: simplify database migrations

--- ldes-server-infra-postgres/postgres-liquibase/src/main/resources/db/changelog/3_4_2/add-collection-stats.sql
+++ ldes-server-infra-postgres/postgres-liquibase/src/main/resources/db/changelog/3_4_2/add-collection-stats.sql
@@ -28,15 +28,50 @@ from members m join collections c on m.collection_id = c.collection_id
 group by c.collection_id;
 
 -- create function to update collection stats when member ingested
-create function on_member_inserted() returns trigger language plpgsql as $$
+create function on_members_inserted() returns trigger language plpgsql as $$
+declare
+  _stats collection_stats%ROWTYPE;
 begin
-  update collection_stats
-  set ingested_count = ingested_count + 1, ingested_last_id = NEW.member_id
-  where collection_id = NEW.collection_id;
+  for _stats in
+    select collection_id, count(member_id)::bigint, max(member_id)
+    from ingested
+    group by collection_id
+  loop
+    update collection_stats set
+      ingested_count = ingested_count + _stats.ingested_count,
+      ingested_last_id = _stats.ingested_last_id
+    where collection_id = _stats.collection_id;
+  end loop;
   return null;
 end
 $$;
 
--- update stats row after member inserted
-create trigger member_ai after insert on members
-for each row execute procedure on_member_inserted();
+-- update stats row after members inserted
+create trigger members_ai after insert on members
+referencing new table as ingested
+for statement execute procedure on_members_inserted();
+
+-- drop needs_bucketization and related
+DROP VIEW "unprocessed_views";
+DROP VIEW "needs_bucketization";
+
+-- use collection stats for needs bucketization
+CREATE OR REPLACE VIEW "needs_bucketization" AS
+SELECT cs.collection_id, v.view_id, (pm.member_id is null) AS should_bucketize
+from collection_stats cs
+join views v on v.collection_id = cs.collection_id
+left outer join page_members pm on pm.view_id = v.view_id and pm.member_id = cs.ingested_last_id
+group by cs.collection_id, v.view_id, pm.member_id;
+
+-- recreate unprocessed_views
+CREATE OR REPLACE VIEW "unprocessed_views" AS
+SELECT c.collection_id, c.name AS collection_name, v.view_id, v.name AS view_name
+FROM collections c
+JOIN views v ON c.collection_id = v.collection_id
+JOIN needs_bucketization nb ON nb.collection_id = c.collection_id AND nb.view_id = v.view_id
+JOIN needs_pagination np ON np.collection_id = c.collection_id AND np.view_id = v.view_id
+WHERE nb.should_bucketize OR np.should_paginate;
+
+-- drop obsolete member and bucket stats
+DROP VIEW "member_stats";
+DROP VIEW "bucket_stats";
--- ldes-server-infra-postgres/postgres-liquibase/src/main/resources/db/changelog/3_4_2/extend-view-stats.sql
+++ ldes-server-infra-postgres/postgres-liquibase/src/main/resources/db/changelog/3_4_2/extend-view-stats.sql
@@ -11,28 +11,59 @@ UPDATE view_stats vs SET paginated_last_id = coalesce((
   select max(distinct(member_id)) from page_members pm WHERE pm.view_id = vs.view_id and pm.page_id is not null
 ), (0)::bigint);
 
--- fix function to also set view stats last bucketized ID
-create or replace function on_page_member_inserted() returns trigger language plpgsql as $$
+-- remove pagination counting triggers and functions
+drop trigger page_member_ai on page_members;
+drop function on_page_member_inserted;
+
+-- create function for pagination counting after page members inserted
+create function on_page_members_inserted() returns trigger language plpgsql as $$
+declare
+  _stats view_stats%ROWTYPE;
 begin
-  if ((select count(member_id) from page_members
-       where view_id = NEW.view_id and member_id = NEW.member_id) = 1) then
-    update view_stats set bucketized_count = bucketized_count + 1 where view_id = NEW.view_id;
-  end if;
-  update view_stats set bucketized_last_id = NEW.member_id where view_id = NEW.view_id;
+  for _stats in
+    select view_id, count(distinct(member_id))::bigint, 0::bigint, max(member_id), 0::bigint
+    from bucketized
+    group by view_id
+  loop
+    update view_stats set
+      bucketized_count = bucketized_count + _stats.bucketized_count,
+      bucketized_last_id = _stats.bucketized_last_id
+    where view_id = _stats.view_id;
+  end loop;
   return null;
 end
 $$;
 
--- fix function to also set view stats last paginated ID
-create or replace function on_page_member_updating() returns trigger language plpgsql as $$
+-- update stats row after page members inserted
+create trigger page_members_ai after insert on page_members
+referencing new table as bucketized
+for statement execute procedure on_page_members_inserted();
+
+
+-- remove pagination counting triggers and functions
+drop trigger page_member_bu on page_members;
+drop function on_page_member_updating;
+
+-- create function for pagination counting after page members updated
+create function on_page_members_updated() returns trigger language plpgsql as $$
+declare
+  _stats view_stats%ROWTYPE;
 begin
-  if (OLD.page_id is null and NEW.page_id is not null) then
-    if ((select count(member_id) from page_members
-         where view_id = NEW.view_id and member_id = NEW.member_id) = 1) then
-      update view_stats set paginated_count = paginated_count + 1 where view_id = NEW.view_id;
-    end if;
-    update view_stats set paginated_last_id = NEW.member_id where view_id = NEW.view_id;
-  end if;
-  return NEW;
+  for _stats in
+    select view_id, 0::bigint, count(distinct(member_id))::bigint, 0::bigint, max(member_id)
+    from paginated where page_id is not null
+    group by view_id
+  loop
+    update view_stats set
+      paginated_count = paginated_count + _stats.paginated_count,
+      paginated_last_id = _stats.paginated_last_id
+    where view_id = _stats.view_id;
+  end loop;
+  return null;
 end
 $$;
+
+-- update stats row after page members updated
+create trigger page_members_au after update on page_members
+referencing new table as paginated
+for statement execute procedure on_page_members_updated();
