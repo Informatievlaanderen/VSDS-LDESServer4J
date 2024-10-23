ac538b5dafa3c28669ac5c46fb593f3c79676e23
Jonas Bulcke <jonas.bulcke@cegeka.com>
2024-10-23 18:05:40 +0200

    feat: extend converter

--- ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/converter/LocalDateTimeConverterTest.java
+++ ldes-server-domain/src/test/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/converter/LocalDateTimeConverterTest.java
@@ -1,37 +1,44 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.domain.converter;
 
-import org.apache.jena.datatypes.TypeMapper;
-import org.apache.jena.rdf.model.impl.LiteralImpl;
-import org.junit.jupiter.api.Test;
+import org.apache.jena.datatypes.RDFDatatype;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.Literal;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.apache.jena.rdf.model.ResourceFactory.createTypedLiteral;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LocalDateTimeConverterTest {
 
 	private final LocalDateTimeConverter localDateTimeConverter = new LocalDateTimeConverter();
 
-	@Test
-	void test_conversionOfXsdTimeToLocalTime() {
-		LiteralImpl typedLiteral = (LiteralImpl) createTypedLiteral("2022-05-20T09:58:15Z",
-				TypeMapper.getInstance().getTypeByName("http://www.w3.org/2001/XMLSchema#dateTime"));
-
-		LocalDateTime actualLocalDateTime = localDateTimeConverter.getLocalDateTime(typedLiteral);
-
-		LocalDateTime expectedLocalDateTime = LocalDateTime.of(2022, 5, 20, 9, 58, 15);
-		assertEquals(expectedLocalDateTime, actualLocalDateTime);
+	static Stream<RDFDatatype> dataTypes() {
+		return Stream.of(XSDDatatype.XSDdateTime, XSDDatatype.XSDdateTime);
 	}
 
-	@Test
-	void test_conversionOfXsdTimeWithoutTimeZone() {
-		LiteralImpl typedLiteral = (LiteralImpl) createTypedLiteral("2023-04-14T12:10:30.629238",
-				TypeMapper.getInstance().getTypeByName("http://www.w3.org/2001/XMLSchema#dateTime"));
+	@ParameterizedTest
+	@MethodSource("dataTypes")
+	void test_conversionOfXsdTimeToLocalTime(RDFDatatype dataType) {
+		LocalDateTime expectedLocalDateTime = LocalDateTime.of(2022, 5, 20, 9, 58, 15);
+		Literal typedLiteral = createTypedLiteral("2022-05-20T09:58:15Z", dataType);
 
 		LocalDateTime actualLocalDateTime = localDateTimeConverter.getLocalDateTime(typedLiteral);
 
+		assertThat(actualLocalDateTime).isEqualTo(expectedLocalDateTime);
+	}
+
+	@ParameterizedTest
+	@MethodSource("dataTypes")
+	void test_conversionOfXsdTimeWithoutTimeZone(RDFDatatype dataType) {
 		LocalDateTime expectedLocalDateTime = LocalDateTime.of(2023, 4, 14, 12, 10, 30, 629000000);
-		assertEquals(expectedLocalDateTime, actualLocalDateTime);
+		Literal typedLiteral = createTypedLiteral("2023-04-14T12:10:30.629238", dataType);
+
+		LocalDateTime actualLocalDateTime = localDateTimeConverter.getLocalDateTime(typedLiteral);
+
+		assertThat(actualLocalDateTime).isEqualTo(expectedLocalDateTime);
 	}
 }
