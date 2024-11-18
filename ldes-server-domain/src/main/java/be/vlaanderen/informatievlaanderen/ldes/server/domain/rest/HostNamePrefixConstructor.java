c5784ee6b495ad3fde2f85bac40ef8b2ec831d30
Jonas Bulcke <jonas.bulcke@cegeka.com>
2024-11-18 12:16:23 +0100

    fix: bad property name used

--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/rest/HostNamePrefixConstructor.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/rest/HostNamePrefixConstructor.java
@@ -1,15 +1,11 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.domain.rest;
 
-import org.springframework.beans.factory.annotation.Value;
-
 import java.util.Map;
-
-import static be.vlaanderen.informatievlaanderen.ldes.server.domain.constants.ServerConfig.HOST_NAME_KEY;
 
 public class HostNamePrefixConstructor implements UriPrefixConstructor {
 	private final String hostname;
 
-	public HostNamePrefixConstructor(@Value(HOST_NAME_KEY) String hostname) {
+	public HostNamePrefixConstructor(String hostname) {
 		this.hostname = hostname;
 	}
 
--- ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/rest/HostNamePrefixConstructorConfig.java
+++ ldes-server-domain/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/domain/rest/HostNamePrefixConstructorConfig.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static be.vlaanderen.informatievlaanderen.ldes.server.domain.constants.ServerConfig.HOST_NAME_KEY;
+
 @Configuration
 public class HostNamePrefixConstructorConfig {
 	@Bean
 	@ConditionalOnMissingBean(UriPrefixConstructor.class)
-	public UriPrefixConstructor hostNamePrefixConstructor(@Value("${ldes.server.hostname}") String hostname) {
+	public UriPrefixConstructor hostNamePrefixConstructor(@Value(HOST_NAME_KEY) String hostname) {
 		return new HostNamePrefixConstructor(hostname);
 	}
 }
