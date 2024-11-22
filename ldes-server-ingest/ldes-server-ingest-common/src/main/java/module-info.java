d7261511550015a7cdc935591ddc9df0c8091698
Yalz <Yalz@users.noreply.github.com>
2024-11-22 15:28:18 +0100

    fix: foreign key

--- ldes-server-ingest/ldes-server-ingest-common/src/main/java/module-info.java
+++ ldes-server-ingest/ldes-server-ingest-common/src/main/java/module-info.java
@@ -8,7 +8,8 @@ module ldes.ingest.domain {
     requires org.apache.jena.arq;
     requires micrometer.core;
     requires micrometer.observation;
-    exports be.vlaanderen.informatievlaanderen.ldes.server.ingest.entities;
+	requires spring.core;
+	exports be.vlaanderen.informatievlaanderen.ldes.server.ingest.entities;
     exports be.vlaanderen.informatievlaanderen.ldes.server.ingest.repositories;
     exports be.vlaanderen.informatievlaanderen.ldes.server.ingest;
 }
