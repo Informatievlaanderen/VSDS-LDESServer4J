915c82489713a7480d71952b8bb084f6a3a1caff
Jonas Bulcke <127748878+jobulcke@users.noreply.github.com>
2024-11-28 15:51:30 +0100

    fix: wrong view stats update (#1431)
    * fix: wrong member pagination count in view stats
    chore: cleanup fragmentation service
    
    * fix: broken test
    
    * chore: implement PR remarks
    chore: add extra tests

--- ldes-server-fragmentation/ldes-server-fragmentation-common/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentation/FragmentationJobScheduler.java
+++ ldes-server-fragmentation/ldes-server-fragmentation-common/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/fragmentation/FragmentationJobScheduler.java
@@ -3,13 +3,14 @@ package be.vlaanderen.informatievlaanderen.ldes.server.fragmentation;
 import be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.entities.UnprocessedView;
 import be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.exceptions.FragmentationJobException;
 import be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.repository.UnprocessedViewRepository;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.builder.SimpleJobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -21,12 +22,11 @@ import java.util.Objects;
 
 import static be.vlaanderen.informatievlaanderen.ldes.server.domain.constants.ServerConfig.FRAGMENTATION_CRON;
 import static be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.batch.BatchConfiguration.ASYNC_JOB_LAUNCHER;
-import static be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.batch.BucketJobDefinitions.BUCKETISATION_STEP;
+import static be.vlaanderen.informatievlaanderen.ldes.server.fragmentation.batch.FragmentationJobDefintions.FRAGMENTATION_JOB;
 
 @Service
 @EnableScheduling
-public class FragmentationService {
-	public static final String FRAGMENTATION_JOB = "fragmentation";
+public class FragmentationJobScheduler {
 	public static final String COLLECTION_NAME = "collectionName";
 	public static final String VIEW_NAME = "viewName";
 	public static final String COLLECTION_ID = "collectionId";
@@ -34,29 +34,27 @@ public class FragmentationService {
 	public static final String LDES_SERVER_CREATE_FRAGMENTS_COUNT = "ldes_server_create_fragments_count";
 	private final JobLauncher jobLauncher;
 	private final JobExplorer jobExplorer;
-	private final JobRepository jobRepository;
-	private final Job bucketiseJob;
+	private final Job fragmentationJob;
 	private final UnprocessedViewRepository unprocessedViewRepository;
 
-	public FragmentationService(@Qualifier(ASYNC_JOB_LAUNCHER) JobLauncher jobLauncher, JobRepository jobRepository, JobExplorer jobExplorer,
-	                            @Qualifier(BUCKETISATION_STEP) Step bucketiseMembersStep, Step paginationStep,
-	                            UnprocessedViewRepository unprocessedViewRepository) {
+	public FragmentationJobScheduler(@Qualifier(ASYNC_JOB_LAUNCHER) JobLauncher jobLauncher,
+	                                 JobExplorer jobExplorer,
+	                                 SimpleJobBuilder fragmentationJobBuilder,
+	                                 UnprocessedViewRepository unprocessedViewRepository) {
 		this.jobLauncher = jobLauncher;
 		this.jobExplorer = jobExplorer;
-		this.jobRepository = jobRepository;
 		this.unprocessedViewRepository = unprocessedViewRepository;
-		this.bucketiseJob = createJob(bucketiseMembersStep, paginationStep);
-		this.cleanupOldJobs();
+		this.fragmentationJob = fragmentationJobBuilder.build();
 	}
 
 	@Scheduled(cron = FRAGMENTATION_CRON)
-	public void scheduledJobLauncher() {
+	public void scheduleJobs() {
 		unprocessedViewRepository.findAll()
 				.parallelStream()
 				.filter(this::noJobsRunning)
 				.forEach(unprocessedView -> {
 					try {
-						jobLauncher.run(bucketiseJob, new JobParametersBuilder()
+						jobLauncher.run(fragmentationJob, new JobParametersBuilder()
 								.addLong(VIEW_ID, (long) unprocessedView.viewId())
 								.addLong(COLLECTION_ID, (long) unprocessedView.collectionId())
 								.addString(VIEW_NAME, unprocessedView.viewName())
@@ -83,22 +81,5 @@ public class FragmentationService {
 					);
 					return Objects.equals(fromParams, unprocessedView);
 				});
-	}
-
-	private Job createJob(Step step, Step paginationStep) {
-		return new JobBuilder(FRAGMENTATION_JOB, jobRepository)
-				.start(step)
-				.next(paginationStep)
-				.build();
-	}
-
-	public void cleanupOldJobs() {
-		jobExplorer.findRunningJobExecutions(FRAGMENTATION_JOB).forEach(this::stopJob);
-	}
-
-	private void stopJob(JobExecution jobExecution) {
-		jobExecution.setStatus(BatchStatus.ABANDONED);
-		jobExecution.setEndTime(LocalDateTime.now());
-		jobRepository.update(jobExecution);
 	}
 }
