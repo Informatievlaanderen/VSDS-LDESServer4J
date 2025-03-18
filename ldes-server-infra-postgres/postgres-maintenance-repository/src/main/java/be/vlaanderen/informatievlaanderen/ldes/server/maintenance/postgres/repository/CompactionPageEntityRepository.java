9d18c128946a50e6ba4a874e8af4b9256d8518f7
Yalz <Yalz@users.noreply.github.com>
2025-03-18 13:11:59 +0100

    fix: verify compaction only forms pages that respect page size

--- ldes-server-infra-postgres/postgres-maintenance-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/maintenance/postgres/repository/CompactionPageEntityRepository.java
+++ ldes-server-infra-postgres/postgres-maintenance-repository/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/maintenance/postgres/repository/CompactionPageEntityRepository.java
@@ -14,18 +14,24 @@ import java.util.List;
 public interface CompactionPageEntityRepository extends JpaRepository<CompactionPageEntity, Long> {
 
 	@Query(value = """
-			SELECT p.page_id as fragmentId, COUNT(*) AS size, r.to_page_id AS toPage, p.bucket_id AS bucketId, p.partial_url AS partialUrl
-			FROM pages p JOIN buckets b ON p.bucket_id = b.bucket_id
-			JOIN views v ON b.view_id = v.view_id
-			JOIN page_relations r ON p.page_id = r.from_page_id
-			JOIN collections c on c.collection_id = v.collection_id
+			SELECT p.page_id                                                     as fragmentId,
+			       (SELECT COUNT(*) FROM page_members WHERE page_id = p.page_id) AS size,
+			       r.to_page_id                                                  AS toPage,
+			       p.bucket_id                                                   AS bucketId,
+			       p.partial_url                                                 AS partialUrl
+			FROM pages p
+			         JOIN buckets b ON p.bucket_id = b.bucket_id
+			         JOIN views v ON b.view_id = v.view_id
+			         JOIN page_relations r ON p.page_id = r.from_page_id
+			         JOIN collections c on c.collection_id = v.collection_id
 			WHERE c.name = :collectionName
-			AND v.name = :viewName
-			AND p.expiration IS NULL AND p.immutable
-			AND NOT p.is_root
+			  AND v.name = :viewName
+			  AND p.expiration IS NULL
+			  AND p.immutable
+			  AND NOT p.is_root
+			  AND (SELECT COUNT(*) FROM page_members WHERE page_id = p.page_id) < :capacityPerPage
 			GROUP BY p.page_id, r.to_page_id
-			HAVING COUNT(*) < :capacityPerPage
-			""", nativeQuery = true)
+				""", nativeQuery = true)
 	@Transactional(readOnly = true)
 	List<CompactionCandidateProjection> findCompactionCandidates(@Param("collectionName") String collectionName,
 	                                                             @Param("viewName") String viewName,
